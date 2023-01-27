4ffa30f053f5f215a3f9006d8e14819aae65f515
Fumitoshi Ukai <ukai@google.com>
2023-01-27 11:39:32 +0000

    siso: Add main.go with initial scaffolding.
    Bug: b/266518906
    Change-Id: I7dd6a0a9249c3078c01504ff9b87cf3003f255ce
    Signed-off-by: Philipp Wollermann <philwo@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4189752
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#54216}
    GitOrigin-RevId: e7b5c844eb52666d22ff9bc00e3745ca2ef4c18e

---
+++ main.go
@@ -0,0 +1,73 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"go.chromium.org/luci/common/system/signals"
+	"os"
+	"runtime"
+
+	log "github.com/golang/glog"
+)
+
+// Siso is an experimental build tool.
+
+func main() {
+	flag.Usage = func() {
+		out := flag.CommandLine.Output()
+		fmt.Fprintf(out, "Usage of %s:\n", os.Args[0])
+		fmt.Fprintf(out, "global flags:\n")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+	if flag.NArg() == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+	ctx := context.Background()
+	var err error
+
+	err = sisoMain(ctx, flag.Args())
+
+	if errors.Is(err, flag.ErrHelp) {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		flag.Usage()
+		os.Exit(2)
+	}
+	if err != nil {
+		log.Exitf("Error: %v", err)
+	}
+}
+
+func sisoMain(ctx context.Context, args []string) error {
+	ctx, cancel := context.WithCancel(ctx)
+	defer signals.HandleInterrupt(cancel)()
+
+	// Flush the log on exit to not lose any messages.
+	defer log.Flush()
+
+	// Print a stack trace when a panic occurs.
+	defer func() {
+		if r := recover(); r != nil {
+			const size = 64 << 10
+			buf := make([]byte, size)
+			buf = buf[:runtime.Stack(buf, false)]
+			log.Fatalf("panic: %v\n%s", r, buf)
+		}
+	}()
+
+	var err error
+	// TODO(b/246687010) use subcommands library
+	switch args[0] {
+	default:
+		err = fmt.Errorf("unknown subcommand %q: %w", args[0], flag.ErrHelp)
+	}
+	return err
+}
