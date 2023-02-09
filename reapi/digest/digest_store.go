232821c3cc1286f188f767f6c30c2a2b2e8642c1
Fumitoshi Ukai <ukai@google.com>
2023-02-09 05:14:57 +0000

    siso: Add digest.Store
    Bug: b/266518906
    Signed-off-by: Junji Watanabe <jwata@google.com>
    Change-Id: Iabbb94b982e3a7d1a9ede519628ce8d9876505f7
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4235719
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#54532}
    GitOrigin-RevId: 22e72e43262bf225163a7f3f1b010c49f34a7c3c

---
+++ reapi/digest/digest_store.go
@@ -0,0 +1,54 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package digest
+
+// Store works as an in-memory content addressable storage.
+type Store struct {
+	m map[Digest]Data
+}
+
+// NewStore creates Store.
+func NewStore() *Store {
+	return &Store{
+		m: make(map[Digest]Data),
+	}
+}
+
+// Set sets data to the store.
+func (s *Store) Set(d Data) {
+	s.m[d.Digest()] = d
+}
+
+// Get gets data from store by the digest.
+func (s *Store) Get(digest Digest) (Data, bool) {
+	v, ok := s.m[digest]
+	return v, ok
+}
+
+// GetSource gets source from the store.
+func (s *Store) GetSource(digest Digest) (Source, bool) {
+	v, ok := s.Get(digest)
+	if !ok {
+		return nil, false
+	}
+	return v.source, true
+}
+
+// Size returns the number of digests in the store.
+func (s *Store) Size() int {
+	if s == nil {
+		return 0
+	}
+	return len(s.m)
+}
+
+// List returns a list of the digests of the stored data.
+func (s *Store) List() []Digest {
+	digests := make([]Digest, 0, len(s.m))
+	for k := range s.m {
+		digests = append(digests, k)
+	}
+	return digests
+}
