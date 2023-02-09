d5ab7ab53237d3271ad6ca442e14b970972a0ed7
Fumitoshi Ukai <ukai@google.com>
2023-02-09 07:03:29 +0000

    siso: Add windows command line utilities
    R=jwata@google.com, philwo@google.com, tikuta@chromium.org
    
    Signed-off-by: Richard Wang <richardwa@google.com>
    Bug: b/266518906
    Change-Id: Icbd49cec8f0ea7b3a62a02f85754e4ea49857c37
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4223718
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Cr-Commit-Position: refs/heads/main@{#54534}
    GitOrigin-RevId: 9a10ee1f96fc2688442711a2f63bc5d0ada14335

---
+++ toolsupport/cmdutil/split_posix.go
@@ -0,0 +1,17 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//go:build unix
+
+package cmdutil
+
+import (
+	"fmt"
+	"runtime"
+)
+
+// Split splits cmd.exe's cmdline. Windows only.
+func Split(cmdline string) ([]string, error) {
+	return nil, fmt.Errorf("cmdutil.Split is not supported on %s", runtime.GOOS)
+}
---
+++ toolsupport/cmdutil/split_windows.go
@@ -0,0 +1,36 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//go:build windows
+
+package cmdutil
+
+import (
+	"runtime"
+	"unsafe"
+
+	"golang.org/x/sys/windows"
+)
+
+// Split splits cmd.exe's cmdline.
+func Split(cmdline string) ([]string, error) {
+	var argc int32
+	argsPtr, err := windows.UTF16PtrFromString(cmdline)
+	if err != nil {
+		return nil, err
+	}
+	sysArgv, err := windows.CommandLineToArgv(argsPtr, &argc)
+	if err != nil {
+		return nil, err
+	}
+	defer windows.LocalFree(windows.Handle(unsafe.Pointer(sysArgv)))
+	args := make([]string, argc)
+	for i, v := range (*sysArgv)[:argc] {
+		// (*v) is [8192]uint16, but may be longer?
+		s := unsafe.Slice(&v[0], len(cmdline))
+		args[i] = windows.UTF16ToString(s)
+	}
+	runtime.KeepAlive(argsPtr)
+	return args, nil
+}
