28fce365745eff5cf464ea539806b28d90dfc049
Fumitoshi Ukai <ukai@google.com>
2023-02-09 02:55:03 +0000

    siso: Add digest.{Data, Source} interfaces and implementations
    The naming is adjusted for consistency.
    
    Bug: b/266518906
    Change-Id: I8f64de2533afc679661171129e58208fd94eb565
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4227439
    Reviewed-by: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Junji Watanabe <jwata@google.com>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Cr-Commit-Position: refs/heads/main@{#54527}
    GitOrigin-RevId: c4f5f5db637ee22dcd393c0f47c7496704e0a501

--- reapi/digest/digest.go
+++ reapi/digest/digest.go
@@ -9,11 +9,16 @@
 package digest
 
 import (
+	"bytes"
+	"context"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
+	"io"
+	"os"
 
 	rpb "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
+	"google.golang.org/protobuf/proto"
 )
 
 // Empty is a digest of empty content.
@@ -27,12 +32,21 @@ type Digest struct {
 
 // ofBytes creates a Digest from bytes.
 func ofBytes(b []byte) Digest {
+	d, _ := fromReader(bytes.NewReader(b))
+	return d
+}
+
+// fromReader creates a Digest from io.Reader.
+func fromReader(r io.Reader) (Digest, error) {
 	h := sha256.New()
-	h.Write(b)
+	n, err := io.Copy(h, r)
+	if err != nil {
+		return Digest{}, err
+	}
 	return Digest{
 		Hash:      hex.EncodeToString(h.Sum(nil)),
-		SizeBytes: int64(len(b)),
-	}
+		SizeBytes: n,
+	}, nil
 }
 
 // FromProto converts from digest proto.
@@ -68,4 +82,142 @@ func (d Digest) String() string {
 		return ""
 	}
 	return fmt.Sprintf("%s/%d", d.Hash, d.SizeBytes)
+}
+
+// Source is the interface that opens a data source.
+// It can be remote or local source.
+// If this interface is implemented based on gRPC streaming for remote sources,
+// the caller may need to retry Open/Read/Close in addition.
+type Source interface {
+	// Open returns io.ReadCloser of the source.
+	Open(context.Context) (io.ReadCloser, error)
+
+	// String returns the name of the data source.
+	String() string
+}
+
+// Data is a data instance that consists of Digest and Source.
+// TODO(b/268407930): it may be possible to be merged with Source.
+type Data struct {
+	digest Digest
+	source Source
+}
+
+// NewData creates a Data from source and digest.
+func NewData(src Source, d Digest) Data {
+	return Data{
+		digest: d,
+		source: src,
+	}
+}
+
+// Digest returns the Digest of the data.
+func (d Data) Digest() Digest {
+	return d.digest
+}
+
+// Open opens the data source.
+func (d Data) Open(ctx context.Context) (io.ReadCloser, error) {
+	return d.source.Open(ctx)
+}
+
+// String returns the digest and the source in string format.
+func (d Data) String() string {
+	return fmt.Sprintf("%v %v", d.digest, d.source)
+}
+
+// DataToBytes returns byte values from a Data.
+// Note that it reads all content. It should not be used for large blob.
+func DataToBytes(ctx context.Context, d Data) ([]byte, error) {
+	f, err := d.Open(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return io.ReadAll(f)
+}
+
+// FromProtoMessage creates Data from proto message.
+func FromProtoMessage(m proto.Message) (Data, error) {
+	b, err := proto.Marshal(m)
+	if err != nil {
+		return Data{}, err
+	}
+	return FromBytes(fmt.Sprintf("%T", m), b), nil
+}
+
+// FromBytes creates data from raw byte values.
+func FromBytes(name string, b []byte) Data {
+	return Data{
+		digest: ofBytes(b),
+		source: byteSource{name: name, b: b},
+	}
+}
+
+// byteSource implements Source for in-memory source with raw byte values.
+type byteSource struct {
+	name string
+	b    []byte
+}
+
+func (b byteSource) Open(ctx context.Context) (io.ReadCloser, error) {
+	return io.NopCloser(bytes.NewReader(b.b)), nil
+}
+
+func (b byteSource) String() string {
+	return b.name
+}
+
+// FromLocalFile creates Data from local file source.
+func FromLocalFile(ctx context.Context, src LocalFileSource) (Data, error) {
+	f, err := src.Open(ctx)
+	if err != nil {
+		return Data{}, err
+	}
+	defer f.Close()
+	d, err := fromReader(f)
+	if err != nil {
+		return Data{}, err
+	}
+	return Data{
+		digest: d,
+		source: src,
+	}, nil
+}
+
+// LocalFileSource is a source for local file.
+type LocalFileSource struct {
+	Fname string
+	// TODO(b/266518906): add IOMetrics.
+	// IOMetrics *iometrics.IOMetrics
+}
+
+type localFile struct {
+	*os.File
+	// TODO(b/266518906): add IOMetrics.
+	// m *iometrics.IOMetrics
+	n int
+}
+
+// Read reads the content of the local file.
+func (f *localFile) Read(buf []byte) (int, error) {
+	n, err := f.File.Read(buf)
+	f.n += n
+	return n, err
+}
+
+// Close closes the local file.
+func (f *localFile) Close() error {
+	return f.File.Close()
+}
+
+// Open opens local file.
+func (s LocalFileSource) Open(ctx context.Context) (io.ReadCloser, error) {
+	r, err := os.Open(s.Fname)
+	return &localFile{File: r}, err
+}
+
+// String returns the source name with "file://" prefix.
+func (s LocalFileSource) String() string {
+	return fmt.Sprintf("file://%s", s.Fname)
 }
--- reapi/digest/digest_test.go
+++ reapi/digest/digest_test.go
@@ -5,17 +5,25 @@
 package digest
 
 import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
 	"testing"
 )
+
+// testDigestStr123 is the digest string for []byte{1, 2, 3}.
+const testDigestStr123 = "039058c6f2c0cb492c533b0a4d14ef77cc0f78abccced5287d84a1a2011cfb81/3"
 
 func TestDigest(t *testing.T) {
 	// Regular case
 	b := []byte{1, 2, 3}
 	d := ofBytes(b)
 
-	wantStr := "039058c6f2c0cb492c533b0a4d14ef77cc0f78abccced5287d84a1a2011cfb81/3"
-	if d.String() != wantStr {
-		t.Errorf("ofBytes(%v).String() = %s, want %s", b, d.String(), wantStr)
+	if d.String() != testDigestStr123 {
+		t.Errorf("ofBytes(%v).String() = %s, want %s", b, d.String(), testDigestStr123)
 	}
 
 	p := d.Proto()
@@ -42,5 +50,79 @@ func TestDigest(t *testing.T) {
 	if empty.IsZero() {
 		t.Errorf("ofBytes([]byte{}).IsZero() = true, want false")
 	}
+}
 
+func TestData(t *testing.T) {
+	ctx := context.Background()
+
+	name := "123"
+	b := []byte{1, 2, 3}
+	d := FromBytes(name, b)
+	bFromData, err := DataToBytes(ctx, d)
+	if err != nil {
+		t.Fatalf("FromBytes(..., []byte{1, 2, 3}).Bytes(ctx) = _, %v, want nil error", err)
+	}
+	if !bytes.Equal(bFromData, b) {
+		t.Errorf("FromBytes(..., []byte{1, 2, 3}).Bytes(ctx) = %v, _, want %v", bFromData, b)
+	}
+	gotStr := d.String()
+	wantStr := fmt.Sprintf("%s 123", testDigestStr123)
+	if gotStr != wantStr {
+		t.Errorf("FromBytes(%q, []byte{1, 2, 3}).Bytes(ctx) = %v, _, want %v", name, gotStr, wantStr)
+	}
+
+	// TODO(b/267409605): Add test for FromProtoMessage.
+}
+
+func TestByteSource(t *testing.T) {
+	ctx := context.Background()
+
+	name := "123"
+	b := []byte{1, 2, 3}
+	bs := byteSource{name, b}
+	gotStr := bs.String()
+	wantStr := "123"
+	if gotStr != wantStr {
+		t.Errorf("byteSource{%q, ...}.String() = %v, want %v", name, gotStr, wantStr)
+	}
+	rc, err := bs.Open(ctx)
+	if err != nil {
+		t.Fatalf("byteSource{...}.Open() = _, %v, want nil error", err)
+	}
+	defer rc.Close()
+
+	gotBytes, _ := io.ReadAll(rc)
+	if !bytes.Equal(gotBytes, b) {
+		t.Errorf("rc.Read(gotBytes) sets %v to gotBytes, want %v", gotBytes, b)
+	}
+}
+
+func TestLocalFileSource(t *testing.T) {
+	ctx := context.Background()
+
+	dir := t.TempDir()
+	fname := filepath.Join(dir, "123")
+	b := []byte{1, 2, 3}
+	err := os.WriteFile(fname, b, 0644)
+	if err != nil {
+		t.Fatalf("failed to write file %q. %v", fname, err)
+	}
+
+	d, err := FromLocalFile(ctx, LocalFileSource{fname})
+	if err != nil {
+		t.Fatalf("FromLocalFile(...) = _, %v, want nil error", err)
+	}
+	gotBytes, err := DataToBytes(ctx, d)
+	if err != nil {
+		t.Fatalf("d.Bytes(...) = _, %v, want nil error", err)
+	}
+	if !bytes.Equal(gotBytes, b) {
+		t.Errorf("d.Bytes(...) = %v, _, want %v", gotBytes, b)
+	}
+
+	gotStr := d.String()
+	wantStr := fmt.Sprintf("%s file://%s", testDigestStr123, fname)
+	if gotStr != wantStr {
+		t.Errorf("d.String() = %q, want %q", gotStr, wantStr)
+	}
 }
