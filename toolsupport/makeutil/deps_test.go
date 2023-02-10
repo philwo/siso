104d3185466d4c66c0a8933b5b15851fbbc44ae8
Richard Wang <richardwa@google.com>
2023-02-10 08:36:22 +0000

    siso: Add test for make depfile parser
    Bug: b/267409605
    Change-Id: I612097021259c952a9880c7604c2d24d4c985eef
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4238275
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Richard Wang <richardwa@google.com>
    Reviewed-by: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Cr-Commit-Position: refs/heads/main@{#54579}
    GitOrigin-RevId: 44e02180f2476225a20cd8d81ab5033eb7410c42

---
+++ toolsupport/makeutil/deps_test.go
@@ -0,0 +1,60 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package makeutil
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestParseDeps(t *testing.T) {
+	for _, tc := range []struct {
+		name     string
+		depsfile []byte
+		want     []string
+	}{
+		{
+			name:     "simple",
+			depsfile: []byte("foo.o:\tbar baz qux"),
+			want: []string{
+				"bar",
+				"baz",
+				"qux",
+			},
+		},
+		{
+			name:     "spaceinname",
+			depsfile: []byte(`foo\ bar.o: baz\ qux`),
+			want: []string{
+				"baz qux",
+			},
+		},
+		{
+			name:     "newlinewhitespaces",
+			depsfile: []byte("foo.o :\tbar\\\n\tbaz\\\r\n  qux"),
+			want: []string{
+				"bar",
+				"baz",
+				"qux",
+			},
+		},
+		{
+			name:     "backslashes",
+			depsfile: []byte("foo\\bar.o: baz\\qux\\\n  quux\\corge"),
+			want: []string{
+				`baz\qux`,
+				`quux\corge`,
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			got := ParseDeps(tc.depsfile)
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Errorf("ParseDeps(%q) -want +got:\n%s", tc.depsfile, diff)
+			}
+		})
+	}
+}
