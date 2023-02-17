bb5e7be0076f345190b14c37b303c085f41b5f84
Takuto Ikuta <tikuta@chromium.org>
2023-02-17 06:02:32 +0000

    siso: import retry package
    Bug: b/266518906
    Change-Id: I15c1e01f2a34732702a04e180ed79a5c91f29816
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4262290
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Richard Wang <richardwa@google.com>
    Cr-Commit-Position: refs/heads/main@{#54766}
    GitOrigin-RevId: eef0f950bdfc2e3d15d3c1553f7bdc5720138509

---
+++ reapi/retry/retry.go
@@ -0,0 +1,53 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package retry provides retrying functionalities.
+package retry
+
+import (
+	"context"
+
+	"go.chromium.org/luci/common/errors"
+	"go.chromium.org/luci/common/retry"
+	"go.chromium.org/luci/common/retry/transient"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func retriableError(err error, called int) bool {
+	st, ok := status.FromError(err)
+	if !ok {
+		st = status.FromContextError(err)
+	}
+
+	switch st.Code() {
+	case codes.ResourceExhausted,
+		codes.Internal,
+		codes.Unavailable:
+		return true
+	case
+		// may get
+		// code = Unauthenticated desc = Request had invalid authentication credentials.
+		// Expected OAuth 2 access token, login cookie or other valid authentication credential.
+		// See https://developers.google.com/identity/sign-in/web/devconsole-project.
+		// (access token expired, need to refresh).
+		// but should not retry if it gets in the first request (wrong auth?)
+		codes.Unauthenticated:
+		return called != 1
+	}
+	return false
+}
+
+// Do calls function `f` and retries with exponential backoff for errors that are known to be retriable.
+func Do(ctx context.Context, f func() error) error {
+	called := 0
+	return retry.Retry(ctx, transient.Only(retry.Default), func() error {
+		called += 1
+		err := f()
+		if retriableError(err, called) {
+			return errors.Annotate(err, "retriable error").Tag(transient.Tag).Err()
+		}
+		return err
+	}, nil)
+}
