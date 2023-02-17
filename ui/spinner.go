0e532bf63c5de3d80099d13d160168cc99c61152
Fumitoshi Ukai <ukai@google.com>
2023-02-17 08:19:59 +0000

    siso: import ui
    R=jwata@google.com, philwo@google.com, tikuta@chromium.org
    
    Bug: b/266518906
    Change-Id: I418f7a802f1b91b77312f507efbd6dcb72f1cf1a
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4262291
    Commit-Queue: Richard Wang <richardwa@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#54768}
    GitOrigin-RevId: 99c7b431fcdf1ff05e106b64b7031c4a054e2408

---
+++ ui/spinner.go
@@ -0,0 +1,61 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ui
+
+import (
+	"fmt"
+	"time"
+)
+
+// Spinner manages a spinner for long operation.
+type Spinner struct {
+	quit, done chan struct{}
+	started    time.Time
+	chars      []byte
+	n          int
+}
+
+// Start starts the spinner.
+func (s *Spinner) Start(format string, args ...interface{}) {
+	s.started = time.Now()
+	fmt.Printf(format, args...)
+	if !IsTerminal {
+		return
+	}
+	if len(s.chars) == 0 {
+		s.chars = []byte(`/-\|`)
+	}
+	s.quit = make(chan struct{})
+	s.done = make(chan struct{})
+	fmt.Printf(" ")
+	go func() {
+		defer close(s.done)
+		for {
+			select {
+			case <-s.quit:
+				return
+			case <-time.After(1 * time.Second):
+				fmt.Printf("\b%c", s.chars[s.n])
+				s.n++
+				if s.n >= len(s.chars) {
+					s.n = 0
+				}
+			}
+		}
+	}()
+}
+
+// Stop stops the spinner.
+func (s *Spinner) Stop(err error) {
+	if IsTerminal {
+		close(s.quit)
+		<-s.done
+	}
+	if err != nil {
+		fmt.Printf("\bfailed %s %v\n", time.Since(s.started), err)
+		return
+	}
+	fmt.Printf("\bdone %s\n", time.Since(s.started))
+}
---
+++ ui/ui.go
@@ -0,0 +1,75 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Package ui provides user interface functionalities.
+package ui
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+
+	"golang.org/x/term"
+)
+
+var (
+	// IsTerminal indicates the stdout is terminal or not.
+	IsTerminal = term.IsTerminal(int(os.Stdout.Fd()))
+	width      int
+)
+
+func init() {
+	if !IsTerminal {
+		return
+	}
+	width, _, _ = term.GetSize(int(os.Stdout.Fd()))
+}
+
+// PrintLines prints message lines.
+// If msgs starts with \n, it will print from the current line.
+// Otherwise, it will replaces the last N lines, where N is len(msgs).
+func PrintLines(msgs ...string) {
+	if !IsTerminal {
+		var buf bytes.Buffer
+		for i, msg := range msgs {
+			if i > 0 {
+				fmt.Fprint(&buf, "\t")
+			}
+			fmt.Fprint(&buf, msg)
+		}
+		fmt.Fprintln(&buf)
+		os.Stdout.Write(buf.Bytes())
+		return
+	}
+	var buf bytes.Buffer
+	if len(msgs) > 0 && msgs[0] == "\n" {
+		msgs = msgs[1:]
+	} else {
+		// Clear the last N lines, where N is len(msgs).
+		for i := 0; i < len(msgs)-1; i++ {
+			fmt.Fprintf(&buf, "\r\033[K\033[A")
+		}
+		fmt.Fprintf(&buf, "\r\033[K")
+
+	}
+	for i, msg := range msgs {
+		if msg == "" {
+			continue
+		}
+		// Truncate in middle if too long, unless terminated with newline.
+		// If printing last message, don't truncate if any newline.
+		if width > 4 && len(msg)+3 > width-1 && ((width-4)/2) < len(msg) &&
+			((i < len(msgs)-1 && !strings.Contains(msg[:len(msg)-1], "\n")) ||
+				(i == len(msgs)-1 && !strings.Contains(msg, "\n"))) {
+			n := (width - 4) / 2
+			msg = msg[:n] + "..." + msg[len(msg)-n:]
+		}
+		if i > 0 {
+			fmt.Fprintln(&buf)
+		}
+		fmt.Fprint(&buf, msg)
+	}
+	os.Stdout.Write(buf.Bytes())
+}
