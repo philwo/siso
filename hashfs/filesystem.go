826e960ad48e2036c1ea55113086c09a011a60e4
Takuto Ikuta <tikuta@chromium.org>
2023-03-03 03:29:27 +0000

    siso: make private struct/fields public during migration of hashfs
    I'll use these from internal repository during migration.
    
    Bug: b/266518906
    Change-Id: If0bf41ef1ae02860d752c85c72e921c0e6232c36
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4304725
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#55113}
    GitOrigin-RevId: bf4ac49be1b8256f69a1ab4fb4f774fc199d9f7f

--- hashfs/filesystem.go
+++ hashfs/filesystem.go
@@ -12,22 +12,23 @@ import (
 // File implements https://pkg.go.dev/io/fs#File.
 // This is an in-memory representation of the file.
 type File struct {
-	buf []byte
-	fi  *FileInfo
+	// TODO(b/266518906): make these fields private.
+	Buf []byte
+	Fi  *FileInfo
 }
 
 // Stat returns a FileInfo describing the file.
 func (f *File) Stat() (fs.FileInfo, error) {
-	return f.fi, nil
+	return f.Fi, nil
 }
 
 // Read reads contents from the file.
 func (f *File) Read(buf []byte) (int, error) {
-	if len(f.buf) == 0 {
+	if len(f.Buf) == 0 {
 		return 0, io.EOF
 	}
-	n := copy(buf, f.buf)
-	f.buf = f.buf[n:]
+	n := copy(buf, f.Buf)
+	f.Buf = f.Buf[n:]
 	return n, nil
 }
 
@@ -38,13 +39,14 @@ func (f *File) Close() error {
 
 // Dir implements https://pkg.go.dev/io/fs#ReadDirFile.
 type Dir struct {
-	ents []DirEntry
-	fi   *FileInfo
+	// TODO(b/266518906): make these fields private.
+	Ents []DirEntry
+	Fi   *FileInfo
 }
 
 // Stat returns a FileInfo describing the directory.
 func (d *Dir) Stat() (fs.FileInfo, error) {
-	return d.fi, nil
+	return d.Fi, nil
 }
 
 // Read reads contents from the dir (permission denied).
@@ -62,20 +64,20 @@ func (d *Dir) Close() error {
 func (d *Dir) ReadDir(n int) ([]fs.DirEntry, error) {
 	if n <= 0 {
 		var ents []fs.DirEntry
-		for _, e := range d.ents {
+		for _, e := range d.Ents {
 			ents = append(ents, e)
 		}
-		d.ents = nil
+		d.Ents = nil
 		return ents, nil
 	}
 	var ents []fs.DirEntry
 	var i int
 	var e DirEntry
-	for i, e = range d.ents {
+	for i, e = range d.Ents {
 		ents = append(ents, e)
 	}
-	d.ents = d.ents[i:]
-	if len(d.ents) == 0 {
+	d.Ents = d.Ents[i:]
+	if len(d.Ents) == 0 {
 		return ents, io.EOF
 	}
 	return ents, nil
--- hashfs/fs.go
+++ hashfs/fs.go
@@ -15,14 +15,15 @@ import (
 	"infra/build/siso/reapi/merkletree"
 )
 
-type entry struct {
-	mtime time.Time
+// TODO(b/266518906): make this struct and its fields private.
+type Entry struct {
+	Mtime time.Time
 
-	d            digest.Digest
-	isExecutable bool
-	target       string // symlink
+	D            digest.Digest
+	IsExecutable bool
+	Target       string // symlink
 
-	data digest.Data // from local
+	Data digest.Data // from local
 }
 
 type HashFS struct {
@@ -30,32 +31,33 @@ type HashFS struct {
 
 // FileInfo implements https://pkg.go.dev/io/fs#FileInfo.
 type FileInfo struct {
-	fname string
-	e     *entry
+	// TODO(b/266518906): make these fields private.
+	Fname string
+	E     *Entry
 }
 
 // Name is a base name of the file.
 func (fi *FileInfo) Name() string {
-	return filepath.Base(fi.fname)
+	return filepath.Base(fi.Fname)
 }
 
 // Size is a size of the file.
 func (fi *FileInfo) Size() int64 {
-	if fi.e.d.IsZero() {
+	if fi.E.D.IsZero() {
 		return 0
 	}
-	return fi.e.d.SizeBytes
+	return fi.E.D.SizeBytes
 }
 
 // Mode is a file mode of the file.
 func (fi *FileInfo) Mode() fs.FileMode {
 	mode := fs.FileMode(0644)
-	if fi.e.d.IsZero() && fi.e.target == "" {
+	if fi.E.D.IsZero() && fi.E.Target == "" {
 		mode |= fs.ModeDir
-	} else if fi.e.d.IsZero() && fi.e.target != "" {
+	} else if fi.E.D.IsZero() && fi.E.Target != "" {
 		mode |= fs.ModeSymlink
 	}
-	if fi.e.isExecutable {
+	if fi.E.IsExecutable {
 		mode |= 0111
 	}
 	return mode
@@ -63,48 +65,49 @@ func (fi *FileInfo) Mode() fs.FileMode {
 
 // ModTime is a modification time of the file.
 func (fi *FileInfo) ModTime() time.Time {
-	return fi.e.mtime
+	return fi.E.Mtime
 }
 
 // IsDir returns true if it is the directory.
 func (fi *FileInfo) IsDir() bool {
 	// TODO: e.directory != nil?
-	return fi.e.d.IsZero() && fi.e.target == ""
+	return fi.E.D.IsZero() && fi.E.Target == ""
 }
 
 // Sys returns merkletree Entry of the file.
 func (fi *FileInfo) Sys() any {
 	return merkletree.Entry{
-		Name:         fi.fname,
-		Data:         fi.e.data,
-		IsExecutable: fi.e.isExecutable,
-		Target:       fi.e.target,
+		Name:         fi.Fname,
+		Data:         fi.E.Data,
+		IsExecutable: fi.E.IsExecutable,
+		Target:       fi.E.Target,
 	}
 }
 
 // DirEntry implements https://pkg.go.dev/io/fs#DirEntry.
 type DirEntry struct {
-	fi *FileInfo
+	// TODO(b/266518906): make this private.
+	Fi *FileInfo
 }
 
 // Name is a base name in the directory.
 func (de DirEntry) Name() string {
-	return de.fi.Name()
+	return de.Fi.Name()
 }
 
 // IsDir returns true if it is a directory.
 func (de DirEntry) IsDir() bool {
-	return de.fi.IsDir()
+	return de.Fi.IsDir()
 }
 
 // Type returns a file type.
 func (de DirEntry) Type() fs.FileMode {
-	return de.fi.Mode().Type()
+	return de.Fi.Mode().Type()
 }
 
 // Info returns a FileInfo.
 func (de DirEntry) Info() (fs.FileInfo, error) {
-	return de.fi, nil
+	return de.Fi, nil
 }
 
 // Entries gets merkletree entries for inputs at root.
