714232c208039505a0cd7d071fbf6aa141b484e8
Fumitoshi Ukai <ukai@google.com>
2023-03-13 05:51:51 +0000

    siso: add build/pprof.go
    Bug: b/266518906
    Change-Id: I4ac0ff8a87f5db53d5a4b60f19274c95764c2bf4
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4328556
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Cr-Commit-Position: refs/heads/main@{#55335}
    GitOrigin-RevId: e54018c63a5d5700792acd35aaae2c8cf362b67e

--- build/builder.go
+++ build/builder.go
@@ -1,14 +1,26 @@
-// Copyright 2023 The Chromium Authors. All rights reserved.
+// Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 package build
 
-// logLabelKeyID is a key of logging label for step ID.
-const logLabelKeyID = "id"
+// logging labels's key.
+const (
+	logLabelKeyID        = "id"
+	logLabelKeyBacktrace = "backtrace"
+)
 
 // Builder is a builder.
 type Builder struct {
 	stepDefs      StepDefs
 	sharedDepsLog SharedDepsLog
+}
+
+// Metadata is a metadata of the build process.
+type Metadata struct {
+	// KV is an arbitrary key-value pair in the metadata.
+	KV     map[string]string
+	NumCPU int
+	GOOS   string
+	GOARCH string
 }
---
+++ build/pprof.go
@@ -0,0 +1,92 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package build
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"infra/build/siso/o11y/clog"
+	"infra/build/siso/o11y/pprof"
+	"infra/build/siso/o11y/trace"
+)
+
+type tracePprof struct {
+	fname string
+
+	p *pprof.Profile
+}
+
+// newTracePprof creates new *tracePprof to record a file specified by fname.
+// If fname is empty, *tracePprof does nothing.
+func newTracePprof(fname string) *tracePprof {
+	return &tracePprof{
+		fname: fname,
+		p:     pprof.New("build"),
+	}
+}
+
+func (tp *tracePprof) Close(ctx context.Context) error {
+	if tp.fname == "" {
+		return nil
+	}
+	clog.Infof(ctx, "pprof write to %s", tp.fname)
+	w, err := os.Create(tp.fname)
+	if err != nil {
+		return err
+	}
+	err = tp.p.WriteTo(w, 0)
+	if err != nil {
+		w.Close()
+		return err
+	}
+	return w.Close()
+}
+
+func (tp *tracePprof) Add(ctx context.Context, tc *trace.Context) {
+	if tp.fname == "" {
+		return
+	}
+	spans := tc.Spans()
+	if len(spans) == 0 {
+		return
+	}
+	backtraces, _ := spans[0].Attrs[logLabelKeyBacktrace].([]string)
+
+	for _, span := range spans[1:] {
+		switch span.Name {
+		case "serv:preproc",
+			"serv:localexec",
+			"serv:remoteexec",
+			"serv:remotecache",
+			"serv:localcache-digest",
+			"serv:remoteexec-digest":
+			bt := append([]string{span.Name}, backtraces...)
+			tp.p.Add(span.Duration(), bt)
+		case "serv:deps-gcc", "serv:deps-msvc":
+			bt := append([]string{span.Name, "serv:preproc"}, backtraces...)
+			tp.p.Add(span.Duration(), bt)
+		case "rbe:queue", "rbe:worker":
+			bt := append([]string{span.Name, "serv:remoteexec"}, backtraces...)
+			tp.p.Add(span.Duration(), bt)
+		case "rbe:input", "rbe:exec", "rbe:output":
+			bt := append([]string{span.Name, "rbe:worker", "serv:remoteexec"}, backtraces...)
+			tp.p.Add(span.Duration(), bt)
+		}
+	}
+}
+
+func (tp *tracePprof) SetMetadata(metadata Metadata) {
+	if tp.fname == "" {
+		return
+	}
+	for k, v := range metadata.KV {
+		tp.p.AddComment(fmt.Sprintf("%s=%q", k, v))
+	}
+	tp.p.AddComment(fmt.Sprintf("num_cpu=%d", metadata.NumCPU))
+	tp.p.AddComment(fmt.Sprintf("goos=%s", metadata.GOOS))
+	tp.p.AddComment(fmt.Sprintf("goarch=%s", metadata.GOARCH))
+}
