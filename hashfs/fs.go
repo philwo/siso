b69269f5e3166ba89cfe3c91c8fc5bf9ec49ed80
Fumitoshi Ukai <ukai@google.com>
2023-03-13 07:20:13 +0000

    siso: import methods for entry in hashfs
    Signed-off-by: Takuto Ikuta <tikuta@chromium.org>
    Bug: b/266518906
    Change-Id: I265471873a1057e44e32ef79f16732ebaed747b6
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4328557
    Reviewed-by: Philipp Wollermann <philwo@google.com>
    Cr-Commit-Position: refs/heads/main@{#55337}
    GitOrigin-RevId: 53db7711c2543d8f4dbee6efd5a25361600841e9

--- hashfs/fs.go
+++ hashfs/fs.go
@@ -7,16 +7,46 @@ package hashfs
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io/fs"
+	"os"
 	"path/filepath"
+	"runtime"
 	"sync"
 	"sync/atomic"
 	"time"
 
+	log "github.com/golang/glog"
+
+	"infra/build/siso/o11y/clog"
+	"infra/build/siso/o11y/iometrics"
 	"infra/build/siso/reapi/digest"
 	"infra/build/siso/reapi/merkletree"
 )
+
+func localDigest(ctx context.Context, fname string, m *iometrics.IOMetrics) (digest.Data, error) {
+	// TODO(b/271059955): add xattr support.
+	src := digest.LocalFileSource{Fname: fname, IOMetrics: m}
+	return digest.FromLocalFile(ctx, src)
+}
+
+func isSymlink(m fs.FileMode) bool {
+	return m.Type() == fs.ModeSymlink
+}
+
+func isExecutable(fi fs.FileInfo, fname string) bool {
+	if fi.Mode()&0111 != 0 {
+		return true
+	}
+	if runtime.GOOS != "windows" {
+		return false
+	}
+	// siso-toolchain-chromium-browser-clang creates an executables
+	// marker file, so check it.
+	_, err := os.Stat(fname + ".is_executable")
+	return err == nil
+}
 
 // TODO(b/266518906): make this struct and its fields private.
 type Entry struct {
@@ -55,10 +85,21 @@ type Entry struct {
 	Err       error
 }
 
+// TODO(b/266518906): make this private.
+func NewLocalEntry() *Entry {
+	lready := make(chan bool)
+	close(lready)
+	readyq := make(chan struct{})
+	return &Entry{
+		Lready: lready,
+		Readyq: readyq,
+	}
+}
+
 func (e *Entry) String() string {
 	if e.Ready.Load() {
 		switch {
-		case GetDir(e) != nil:
+		case e.GetDir() != nil:
 			return "<directory>"
 		case e.Target != "":
 			return "<symlink>"
@@ -69,13 +110,270 @@ func (e *Entry) String() string {
 	return "<not ready>"
 }
 
+// TODO(b/266518906): make this private.
+func (e *Entry) Init(ctx context.Context, fname string, m *iometrics.IOMetrics) {
+	defer func() {
+		close(e.Readyq)
+		e.Ready.Store(true)
+	}()
+	fi, err := os.Lstat(fname)
+	m.OpsDone(err)
+	if errors.Is(err, fs.ErrNotExist) {
+		clog.Infof(ctx, "not exist %s", fname)
+		e.SetError(err)
+		return
+	}
+	if err != nil {
+		clog.Warningf(ctx, "failed to lstat %s: %v", fname, err)
+		e.SetError(err)
+		return
+	}
+	switch {
+	case fi.IsDir():
+		if log.V(1) {
+			clog.Infof(ctx, "tree entry %s: is dir", fname)
+		}
+		e.InitDir()
+		// don't update mtime, so updateDir scans local dir.
+		return
+	case isSymlink(fi.Mode()):
+		e.Target, err = os.Readlink(fname)
+		m.OpsDone(err)
+		if err != nil {
+			e.SetError(err)
+		}
+		if log.V(1) {
+			clog.Infof(ctx, "tree entry %s: symlink to %s: %v", fname, e.Target, e.Err)
+		}
+	case fi.Mode().IsRegular():
+		e.Data, err = localDigest(ctx, fname, m)
+		if err != nil {
+			clog.Errorf(ctx, "tree entry %s: file error: %v", fname, err)
+			e.SetError(err)
+			return
+		}
+		e.D = e.Data.Digest()
+		e.IsExecutable = isExecutable(fi, fname)
+		if log.V(1) {
+			clog.Infof(ctx, "tree entry %s: file %s x:%t", fname, e.D, e.IsExecutable)
+		}
+	default:
+		e.SetError(fmt.Errorf("unexpected filetype not regular %s: %s", fi.Mode(), fname))
+		clog.Errorf(ctx, "tree entry %s: unknown filetype %s", fname, fi.Mode())
+		return
+	}
+	if e.Mtime.Before(fi.ModTime()) {
+		e.Mtime = fi.ModTime()
+	}
+}
+
+// TODO(b/266518906): make this private.
+func (e *Entry) WaitReady(ctx context.Context) error {
+	if !e.Ready.Load() {
+		select {
+		case <-e.Readyq:
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+	return nil
+}
+
+// TODO(b/266518906): make this private.
+func (e *Entry) InitDir() {
+	e.Mu.Lock()
+	defer e.Mu.Unlock()
+	if e.Directory == nil {
+		e.Directory = &Directory{}
+	}
+	// reset mtime so updateDir will lookup local dir.
+	e.Mtime = time.Time{}
+	e.Err = nil
+}
+
+// TODO(b/266518906): make this private.
+func (e *Entry) UpdateDir(ctx context.Context, dname string) []string {
+	e.Mu.Lock()
+	defer e.Mu.Unlock()
+	d, err := os.Open(dname)
+	if err != nil {
+		clog.Warningf(ctx, "updateDir %s: open %v", dname, err)
+		return nil
+	}
+	defer d.Close()
+	fi, err := d.Stat()
+	if err != nil {
+		clog.Warningf(ctx, "updateDir %s: stat %v", dname, err)
+		return nil
+	}
+	if !fi.IsDir() {
+		clog.Warningf(ctx, "updateDir %s: is not dir?", dname)
+		return nil
+	}
+	if fi.ModTime().Equal(e.Mtime) {
+		if log.V(1) {
+			clog.Infof(ctx, "updateDir %s: up-to-date %s", dname, e.Mtime)
+		}
+		return nil
+	}
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		clog.Warningf(ctx, "updateDir %s: readdirnames %v", dname, err)
+		return nil
+	}
+	clog.Infof(ctx, "updateDir mtime %s %d %s", dname, len(names), e.Mtime)
+	e.Mtime = fi.ModTime()
+	return names
+}
+
 // GetDir returns directory of entry.
-// TODO(b/266518906): make this Entry's private method.
-func GetDir(e *Entry) *Directory {
+// TODO(b/266518906): make this private.
+func (e *Entry) GetDir() *Directory {
 	if e == nil {
 		return nil
 	}
 	return e.Directory
+}
+
+// TODO(b/266518906): make this private.
+func (e *Entry) SetError(err error) {
+	e.Mu.Lock()
+	defer e.Mu.Unlock()
+	if e.Err == nil {
+		e.Err = err
+	}
+	if e.Err != nil {
+		e.D = digest.Digest{}
+		e.Data = digest.Data{}
+		e.Buf = nil
+		e.Target = ""
+		e.Directory = nil
+	}
+}
+
+// TODO(b/266518906): make this private.
+func (e *Entry) GetError() error {
+	e.Mu.RLock()
+	defer e.Mu.RUnlock()
+	return e.Err
+}
+
+// TODO(b/266518906): make this private.
+func (e *Entry) Flush(ctx context.Context, fname string, m *iometrics.IOMetrics) error {
+	defer close(e.Lready)
+
+	if errors.Is(e.GetError(), fs.ErrNotExist) {
+		err := os.RemoveAll(fname)
+		m.OpsDone(err)
+		clog.Infof(ctx, "flush remove %s: %v", fname, err)
+		return nil
+	}
+	switch {
+	case e.Directory != nil:
+		// directory
+		err := os.MkdirAll(fname, 0755)
+		m.OpsDone(err)
+		clog.Infof(ctx, "flush dir %s: %v", fname, err)
+		e.SetError(err)
+		return err
+	case e.D.IsZero() && e.Target != "":
+		err := os.Symlink(e.Target, fname)
+		m.OpsDone(err)
+		if errors.Is(err, fs.ErrExist) {
+			err = os.Remove(fname)
+			m.OpsDone(err)
+			err = os.Symlink(e.Target, fname)
+			m.OpsDone(err)
+		}
+		clog.Infof(ctx, "flush symlink %s -> %s: %v", fname, e.Target, err)
+		e.SetError(err)
+		// don't change mtimes. it fails if target doesn't exist.
+		return err
+	default:
+	}
+	fi, err := os.Lstat(fname)
+	m.OpsDone(err)
+	if err == nil {
+		if fi.Size() == e.D.SizeBytes && fi.ModTime().Equal(e.Mtime) {
+			// TODO: check hash, mode?
+			clog.Infof(ctx, "flush %s: already exist", fname)
+			return nil
+		}
+		var fileDigest digest.Digest
+		d, err := localDigest(ctx, fname, m)
+		if err == nil {
+			fileDigest = d.Digest()
+			if fileDigest == e.D {
+				clog.Infof(ctx, "flush %s: already exist - hash match", fname)
+				if !fi.ModTime().Equal(e.Mtime) {
+					err = os.Chtimes(fname, time.Now(), e.Mtime)
+					m.OpsDone(err)
+				}
+				return err
+			}
+		}
+		clog.Warningf(ctx, "flush %s: exists but mismatch size:%d!=%d mtime:%s!=%s d:%v!=%v", fname, fi.Size(), e.D.SizeBytes, fi.ModTime(), e.Mtime, fileDigest, e.D)
+		if fi.Mode()&0200 == 0 {
+			// need to be writable. otherwise os.WriteFile fails with permission denied.
+			err = os.Chmod(fname, fi.Mode()|0200)
+			m.OpsDone(err)
+			clog.Warningf(ctx, "flush %s: not writable? %s: %v", fname, fi.Mode(), err)
+		}
+
+	}
+	err = os.MkdirAll(filepath.Dir(fname), 0755)
+	m.OpsDone(err)
+	if err != nil {
+		clog.Warningf(ctx, "flush %s: mkdir: %v", fname, err)
+		return fmt.Errorf("failed to create directory for %s: %w", fname, err)
+	}
+	if e.D.SizeBytes == 0 {
+		clog.Infof(ctx, "flush %s: empty file", fname)
+		err := os.WriteFile(fname, nil, 0644)
+		m.WriteDone(0, err)
+		if err != nil {
+			e.SetError(err)
+			return err
+		}
+		err = os.Chtimes(fname, time.Now(), e.Mtime)
+		m.OpsDone(err)
+		if err != nil {
+			e.SetError(err)
+			return err
+		}
+		return nil
+	}
+	buf := e.Buf
+	if len(buf) == 0 {
+		if e.Data.IsZero() {
+			return fmt.Errorf("no data: retrieve %s: ", fname)
+		}
+		buf, err = digest.DataToBytes(ctx, e.Data)
+		clog.Infof(ctx, "flush %s %s from source: %v", fname, e.D, err)
+		if err != nil {
+			e.SetError(err)
+			return fmt.Errorf("flush %s size=%d: %w", fname, e.D.SizeBytes, err)
+		}
+	} else {
+		clog.Infof(ctx, "flush %s from embedded buf", fname)
+	}
+	mode := os.FileMode(0644)
+	if e.IsExecutable {
+		mode = os.FileMode(0755)
+	}
+	err = os.WriteFile(fname, buf, mode)
+	m.WriteDone(len(buf), err)
+	if err != nil {
+		e.SetError(err)
+		return err
+	}
+	err = os.Chtimes(fname, time.Now(), e.Mtime)
+	m.OpsDone(err)
+	if err != nil {
+		e.SetError(err)
+		return err
+	}
+	return nil
 }
 
 // directory is per-directory entry map to reduce mutex contention.
