c498557e4a5a827e7f19e7fac42b3a8725d88ba9
Fumitoshi Ukai <ukai@google.com>
2023-03-13 07:31:20 +0000

    siso: Add build/experiments.go
    Bug: b/266518906
    Change-Id: I32d7d28abffdaa9854699925bd4edadb2c57fc80
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4333149
    Reviewed-by: Richard Wang <richardwa@google.com>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#55338}
    GitOrigin-RevId: 2af0a89fcac26ebe95a70ce12d39c56e976817dc

---
+++ build/experiments.go
@@ -0,0 +1,111 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package build
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+	"sync"
+
+	log "github.com/golang/glog"
+)
+
+// experiment id -> hint for the experiment (to check more details).
+var knownExperiments = map[string]string{
+	"keep-going-impure":           "check .siso_localexec",
+	"keep-going-handle-error":     "",
+	"no-file-access-trace":        "",
+	"no-fallback":                 "",
+	"no-fast-deps-fallback":       "",
+	"fail-on-stdouterr":           "",
+	"gvisor":                      "",
+	"ignore-missing-local-inputs": "",
+}
+
+type experimentFeature struct {
+	once sync.Once
+}
+
+// Experiments manages experimental features.
+// Experiments are enabled by SISOEXPERIMENTS environment variable.
+// We don't guarantee experiment id in future versions.
+// Unknown experiment id will be ignored.
+type Experiments struct {
+	once sync.Once
+	m    map[string]*experimentFeature
+}
+
+const experimentEnv = "SISOEXPERIMENTS"
+
+func (e *Experiments) init() {
+	if e.m != nil {
+		return
+	}
+	env := os.Getenv(experimentEnv)
+	if env == "" {
+		return
+	}
+	e.m = make(map[string]*experimentFeature)
+	for _, v := range strings.Split(env, ",") {
+		if _, ok := knownExperiments[v]; !ok {
+			log.Warningf("unknown experiment %q. ignored", v)
+			continue
+		}
+		e.m[v] = &experimentFeature{}
+	}
+}
+
+// ShowOnce shows once about enabled experimental features.
+func (e *Experiments) ShowOnce() {
+	e.init()
+	e.once.Do(func() {
+		s := e.String()
+		if s != "" {
+			fmt.Printf("\r\033[K%s", s)
+		}
+	})
+}
+
+func (e *Experiments) String() string {
+	var sb strings.Builder
+	keys := make([]string, 0, len(e.m))
+	for key := range e.m {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	for _, key := range keys {
+		fmt.Fprintf(&sb, "%s=%s enabled\n", experimentEnv, key)
+	}
+	return sb.String()
+}
+
+// Enabled returns true if experimental feature k is enabled, and
+// log error once with its hint if so.
+func (e *Experiments) Enabled(k, format string, args ...interface{}) bool {
+	ex, ok := e.m[k]
+	if !ok {
+		return false
+	}
+	ex.once.Do(func() {
+		log.Errorf(format+" %s\n", append(args, e.Hint(k))...)
+	})
+	return true
+}
+
+// Hint shows hint message for experimental feature k.
+func (e *Experiments) Hint(k string) string {
+	return knownExperiments[k]
+}
+
+// Suggest returns suggest message to enable experimental feature k.
+func (e *Experiments) Suggest(k string) string {
+	hint := knownExperiments[k]
+	if hint != "" {
+		return fmt.Sprintf("need %s=%s or %s", experimentEnv, k, hint)
+	}
+	return fmt.Sprintf("need %s=%s", experimentEnv, k)
+}
