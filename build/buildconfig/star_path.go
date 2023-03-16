99da4e0e43cb9f54b99e02a0dd30bed37dd2c592
Fumitoshi Ukai <ukai@google.com>
2023-03-16 10:01:42 +0000

    siso: add build/buildconfig path module
    Bug: b/266518906
    Change-Id: I8d9d8a0cf1b10e8cbd0f664349415f20002c797d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4344447
    Commit-Queue: Richard Wang <richardwa@google.com>
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Richard Wang <richardwa@google.com>
    Reviewed-by: Philipp Wollermann <philwo@google.com>
    Cr-Commit-Position: refs/heads/main@{#55446}
    GitOrigin-RevId: c8d24dfc63e1a0d8bed2745859025dc6a837c3ac

---
+++ build/buildconfig/star_path.go
@@ -0,0 +1,94 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package buildconfig
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"go.starlark.net/starlark"
+	"go.starlark.net/starlarkstruct"
+)
+
+// starPath returns path module.
+//
+//	base(fname)
+//	dir(fname)
+//	join(...)
+//	rel(basepath, targetpath)
+//	isabs(fname)
+func starPath() starlark.Value {
+	pathModule := &starlarkstruct.Module{
+		Name: "path",
+		Members: map[string]starlark.Value{
+			"base":  starlark.NewBuiltin("base", starPathBase),
+			"dir":   starlark.NewBuiltin("dir", starPathDir),
+			"join":  starlark.NewBuiltin("join", starPathJoin),
+			"rel":   starlark.NewBuiltin("rel", starPathRel),
+			"isabs": starlark.NewBuiltin("isabs", starPathIsAbs),
+		},
+	}
+	pathModule.Freeze()
+	return pathModule
+}
+
+// starlark function `path.base(fname)` to return base name of fname.
+func starPathBase(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var fname string
+	err := starlark.UnpackArgs("base", args, kwargs, "fname", &fname)
+	if err != nil {
+		return starlark.None, err
+	}
+	return starlark.String(filepath.Base(fname)), nil
+}
+
+// starlark function `path.dir(fname)` to return dir name of fname.
+func starPathDir(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var fname string
+	err := starlark.UnpackArgs("dir", args, kwargs, "fname", &fname)
+	if err != nil {
+		return starlark.None, err
+	}
+	return starlark.String(filepath.ToSlash(filepath.Dir(fname))), nil
+}
+
+// starlark function `path.join(...)` to return joined path name.
+func starPathJoin(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var elems []string
+	for _, v := range args {
+		s, ok := starlark.AsString(v)
+		if !ok {
+			return starlark.None, fmt.Errorf("join: for parameter elems: got %s, want string", v.Type())
+		}
+		elems = append(elems, s)
+	}
+	return starlark.String(filepath.ToSlash(filepath.Join(elems...))), nil
+}
+
+// starlark function `path.rel(basepath, targetpath)` to return relative path of targetpath from basepath.
+func starPathRel(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var basepath, targetpath string
+	err := starlark.UnpackArgs("rel", args, kwargs, "basepath", &basepath, "targetpath", &targetpath)
+	if err != nil {
+		return starlark.None, err
+	}
+	rel, err := filepath.Rel(basepath, targetpath)
+	if err != nil {
+		return starlark.None, err
+	}
+	return starlark.String(filepath.ToSlash(rel)), nil
+}
+
+// starlark function `path.isabs(fname)` to return true if fname is absolute path.
+func starPathIsAbs(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var fname string
+	err := starlark.UnpackArgs("isabs", args, kwargs, "fname", &fname)
+	if err != nil {
+		return starlark.None, err
+	}
+	// Should return true for /path or \path on Windows?
+	r := filepath.IsAbs(fname)
+	return starlark.Bool(r), nil
+}
