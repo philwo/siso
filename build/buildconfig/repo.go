e43ff258ef5c6a73b91a5484bcda3f19e977954a
Fumitoshi Ukai <ukai@google.com>
2023-03-16 07:14:09 +0000

    siso: add build/buildconfig/repo
    Bug: b/266518906
    Change-Id: I9edeee383c30121c4b435061fdb0599d04864ed1
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4344443
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#55442}
    GitOrigin-RevId: eae6db5d11ae018cede78434212f4608b3bf61ae

---
+++ build/buildconfig/repo.go
@@ -0,0 +1,100 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package buildconfig
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	log "github.com/golang/glog"
+	"go.starlark.net/starlark"
+
+	"infra/build/siso/o11y/clog"
+)
+
+type emptyFS struct{}
+
+func (emptyFS) Open(name string) (fs.File, error) {
+	return nil, &fs.PathError{Op: "open", Path: name, Err: fs.ErrNotExist}
+}
+
+// repoLoader is a starlark repository loader.
+type repoLoader struct {
+	ctx         context.Context
+	repos       map[string]fs.FS
+	predeclared starlark.StringDict
+}
+
+// Load loads a starlark module.
+// A module may be `@<repo>//` prefix to select repository.
+func (r *repoLoader) Load(thread *starlark.Thread, module string) (starlark.StringDict, error) {
+	curname := thread.Local("modulename").(string)
+	log.V(1).Infof("load %s from %s", module, curname)
+	var curModule string
+	if m, n, ok := strings.Cut(curname, "//"); ok {
+		curModule = m[1:]
+		curname = n
+	}
+	curdir := path.Dir(curname)
+	var moduleName string
+	var fname string
+	var fullname string
+	if !strings.HasPrefix(module, "@") {
+		fname = module
+		if !path.IsAbs(fname) {
+			fname = path.Join(curdir, module)
+		}
+		moduleName = curModule
+	} else {
+		m, n, ok := strings.Cut(module, "//")
+		if !ok {
+			return nil, fmt.Errorf("failed to parse module: %q", module)
+		}
+		moduleName = m[1:]
+		fname = n
+	}
+	if moduleName != "" {
+		fullname = fmt.Sprintf("@%s//%s", moduleName, fname)
+	} else {
+		fullname = fname
+	}
+	log.V(1).Infof("module=%q fname=%q fullname=%q", moduleName, fname, fullname)
+	var buf []byte
+	var err error
+	if moduleName != "" {
+		moduleFS, ok := r.repos[moduleName]
+		if !ok {
+			return nil, fmt.Errorf("no such module defined %q", moduleName)
+		}
+		buf, err = fs.ReadFile(moduleFS, fname)
+	} else {
+		buf, err = os.ReadFile(fname)
+	}
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) && moduleName == "config_overrides" {
+			clog.Warningf(r.ctx, "no @config_overrides//%s: %v", fname, err)
+			name := strings.TrimSuffix(fname, filepath.Ext(fname))
+			return starlark.StringDict(map[string]starlark.Value{
+				name: starlark.None,
+			}), nil
+		}
+		return nil, fmt.Errorf("failed to load %s: %w", fullname, err)
+	}
+	t := &starlark.Thread{
+		Name: "module " + module + "(fullname: " + fullname + ")",
+		Print: func(thread *starlark.Thread, msg string) {
+			log.Infof("thread:%s %s", thread.Name, msg)
+		},
+		Load: r.Load,
+	}
+	t.SetLocal("modulename", fullname)
+	return starlark.ExecFile(t, fname, buf, r.predeclared)
+}
