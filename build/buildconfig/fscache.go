36e373bbd52380deeb6ad7d17f8ec12fede547ae
Fumitoshi Ukai <ukai@google.com>
2023-03-17 08:30:20 +0000

    siso: add build/buildconfig fs module
    Bug: b/266518906
    Change-Id: I728257ef21d27c0491b5407a77c3296ae41f8347
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4348395
    Commit-Queue: Richard Wang <richardwa@google.com>
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Philipp Wollermann <philwo@google.com>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Richard Wang <richardwa@google.com>
    Cr-Commit-Position: refs/heads/main@{#55481}
    GitOrigin-RevId: 6ee47d41b26197d7cb8fbc910dcc0183a6be7795

---
+++ build/buildconfig/fscache.go
@@ -0,0 +1,50 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package buildconfig
+
+import (
+	"context"
+	"io/fs"
+	"sync"
+
+	log "github.com/golang/glog"
+	"golang.org/x/sync/singleflight"
+
+	"infra/build/siso/o11y/clog"
+)
+
+// fscache is a cache of contents from fs (hashfs).
+// TODO(b/273878593): limit cache size in buildconfig/fscache
+type fscache struct {
+	mu sync.Mutex
+	s  singleflight.Group
+	m  map[string][]byte
+}
+
+// Get reads the file fname from fsys into memory and returns its content.
+func (c *fscache) Get(ctx context.Context, fsys fs.FS, fname string) ([]byte, error) {
+	c.mu.Lock()
+	buf, ok := c.m[fname]
+	c.mu.Unlock()
+	if ok {
+		if log.V(1) {
+			clog.Infof(ctx, "fscache hit %s: %d", fname, len(buf))
+		}
+		return buf, nil
+	}
+	v, err, _ := c.s.Do(fname, func() (interface{}, error) {
+		buf, err := fs.ReadFile(fsys, fname)
+		if err != nil {
+			return buf, err
+		}
+		clog.Infof(ctx, "fscache set %s: %d", fname, len(buf))
+		c.mu.Lock()
+		c.m[fname] = buf
+		c.mu.Unlock()
+		return buf, err
+	})
+	buf = v.([]byte)
+	return buf, err
+}
---
+++ build/buildconfig/star_fs.go
@@ -0,0 +1,136 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package buildconfig
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io/fs"
+	"path/filepath"
+
+	log "github.com/golang/glog"
+	"go.starlark.net/starlark"
+	"go.starlark.net/starlarkstruct"
+
+	"infra/build/siso/build"
+)
+
+// starFS returns fs module.
+//
+//	read(fname): reads a file.
+//	is_dir(fname): check if fname is a dir.
+//	exists(fname): check if fname exists.
+//	canonpath(fname): canonicalize path from working dir relative path.
+func starFS(ctx context.Context, fs fs.FS, path *build.Path, fsc *fscache) starlark.Value {
+	receiver := starFSReceiver{
+		ctx:     ctx,
+		fs:      fs,
+		path:    path,
+		fscache: fsc,
+	}
+	fsRead := starlark.NewBuiltin("read", starFSRead).BindReceiver(receiver)
+	fsIsDir := starlark.NewBuiltin("is_dir", starFSIsDir).BindReceiver(receiver)
+	fsExists := starlark.NewBuiltin("exists", starFSExists).BindReceiver(receiver)
+	fsCanonPath := starlark.NewBuiltin("canonpath", starFSCanonPath).BindReceiver(receiver)
+	return starlarkstruct.FromStringDict(starlark.String("fs"), map[string]starlark.Value{
+		"read":      fsRead,
+		"is_dir":    fsIsDir,
+		"exists":    fsExists,
+		"canonpath": fsCanonPath,
+	})
+}
+
+type starFSReceiver struct {
+	ctx     context.Context
+	fs      fs.FS
+	path    *build.Path
+	fscache *fscache
+}
+
+func (r starFSReceiver) String() string {
+	return fmt.Sprintf("fs[%s,%s]", r.path.ExecRoot, r.path.Dir)
+}
+
+func (starFSReceiver) Type() string          { return "fs" }
+func (starFSReceiver) Freeze()               {}
+func (starFSReceiver) Truth() starlark.Bool  { return starlark.True }
+func (starFSReceiver) Hash() (uint32, error) { return 0, errors.New("fs is not hashable") }
+
+// starlark function `fs.read(fname)` to return contents of fname.
+func starFSRead(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	c, ok := fn.Receiver().(starFSReceiver)
+	if !ok {
+		return starlark.None, fmt.Errorf("unexpected receiver: %v", fn.Receiver())
+	}
+	var fname string
+	err := starlark.UnpackArgs("read", args, kwargs, "fname", &fname)
+	if err != nil {
+		return starlark.None, err
+	}
+	buf, err := c.fscache.Get(c.ctx, c.fs, fname)
+	if err != nil {
+		return starlark.None, err
+	}
+	return starlark.Bytes(string(buf)), nil
+}
+
+// starlark function `fs.is_dir(fname)` to check fname is a dir.
+func starFSIsDir(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	c, ok := fn.Receiver().(starFSReceiver)
+	if !ok {
+		return starlark.None, fmt.Errorf("unexpected receiver: %v", fn.Receiver())
+	}
+	var fname string
+	err := starlark.UnpackArgs("is_dir", args, kwargs, "fname", &fname)
+	if err != nil {
+		return starlark.None, err
+	}
+	fi, err := fs.Stat(c.fs, fname)
+	if err != nil {
+		return starlark.None, err
+	}
+	if fi.IsDir() {
+		return starlark.True, nil
+	}
+	return starlark.False, nil
+}
+
+// starlark function `fs.exists(fname)` to check fname exists.
+func starFSExists(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	c, ok := fn.Receiver().(starFSReceiver)
+	if !ok {
+		return starlark.None, fmt.Errorf("unexpected receiver: %v", fn.Receiver())
+	}
+	var fname string
+	err := starlark.UnpackArgs("exists", args, kwargs, "fname", &fname)
+	if err != nil {
+		return starlark.None, err
+	}
+	_, err = fs.Stat(c.fs, fname)
+	if err != nil {
+		return starlark.False, nil
+	}
+	return starlark.True, nil
+}
+
+// starlark function `fs.canonpath(fname)` to canonicalize path from working directory relative path.
+func starFSCanonPath(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	log.V(1).Infof("fs.canonpath args=%s kwargs=%s", args, kwargs)
+	r, ok := fn.Receiver().(starFSReceiver)
+	if !ok {
+		return starlark.None, fmt.Errorf("unexpected receiver: %v", fn.Receiver())
+	}
+	var fname string
+	err := starlark.UnpackArgs("canonpath", args, kwargs, "fname", &fname)
+	if err != nil {
+		return starlark.None, err
+	}
+	s, err := r.path.FromWD(fname)
+	if err != nil {
+		return starlark.None, err
+	}
+	return starlark.String(filepath.ToSlash(s)), nil
+}
