aa071df5a6e383eb7b649db5a1360962f3d03716
Fumitoshi Ukai <ukai@google.com>
2023-03-22 02:46:00 +0000

    siso: add reapi/tree.go
    Bug: b/266518906
    Change-Id: I6ba071ebd5ab180b29fbf3b0c1cda4c17911f0e1
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4355367
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Junji Watanabe <jwata@google.com>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Cr-Commit-Position: refs/heads/main@{#55578}
    GitOrigin-RevId: 1412fb011cc9329aa15b85eb390cc57ca9a2a883

---
+++ reapi/tree.go
@@ -0,0 +1,37 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package reapi
+
+import (
+	"context"
+
+	rpb "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
+	"google.golang.org/protobuf/proto"
+
+	"infra/build/siso/o11y/clog"
+	"infra/build/siso/reapi/digest"
+)
+
+// FetchTree fetches trees at dirname identified by digest into digest store and returns its root directory.
+func (c *Client) FetchTree(ctx context.Context, dirname string, d digest.Digest, ds *digest.Store) (*rpb.Directory, error) {
+	b, err := c.Get(ctx, d, dirname)
+	if err != nil {
+		return nil, err
+	}
+	tree := &rpb.Tree{}
+	err = proto.Unmarshal(b, tree)
+	if err != nil {
+		return nil, err
+	}
+	for _, c := range tree.Children {
+		d, err := digest.FromProtoMessage(c)
+		if err != nil {
+			clog.Errorf(ctx, "digest for children %s: %v", c, err)
+			continue
+		}
+		ds.Set(d)
+	}
+	return tree.Root, nil
+}
