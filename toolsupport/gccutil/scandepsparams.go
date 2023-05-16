f4de60d28a4912e75c8f1f741357a0a6556f6201
Fumitoshi Ukai <ukai@google.com>
2023-05-16 06:55:10 +0000

    siso: add gccutil.ScanDepsParams
    Bug: b/282608727
    Change-Id: I462f571c022efa21091c9e4236532b14c1a114dd
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4535439
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Philipp Wollermann <philwo@google.com>
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Cr-Commit-Position: refs/heads/main@{#56754}
    GitOrigin-RevId: a4ffb8e3097c064f3b96d294e72bd12a4b880bc0

---
+++ toolsupport/gccutil/scandepsparams.go
@@ -0,0 +1,87 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package gccutil
+
+import (
+	"context"
+	"path/filepath"
+	"strings"
+)
+
+// ScanDepsParams parses args and returns files, dirs, sysroots and defines
+// for scandeps.
+// It only parses major command line flags used in chromium.
+// full set of command line flags for include dirs can be found in
+// https://clang.llvm.org/docs/ClangCommandLineReference.html#include-path-management
+func ScanDepsParams(ctx context.Context, args, env []string) (files, dirs, sysroots []string, defines map[string]string, err error) {
+	defines = make(map[string]string)
+	for i := 0; i < len(args); i++ {
+		arg := args[i]
+		if !strings.HasPrefix(arg, "-") {
+			cmdname := filepath.Base(arg)
+			switch {
+			case strings.HasSuffix(cmdname, "clang"),
+				strings.HasSuffix(cmdname, "clang++"),
+				strings.HasSuffix(cmdname, "clang-cl"),
+				strings.HasSuffix(cmdname, "clang-cl.exe"),
+				strings.HasSuffix(cmdname, "gcc"),
+				strings.HasSuffix(cmdname, "g++"):
+				// add toolchain top dir as sysroots too
+				sysroots = append(sysroots, filepath.ToSlash(filepath.Dir(filepath.Dir(arg))))
+			}
+		}
+		switch arg {
+		case "-I", "--include-directory":
+			i++
+			dirs = append(dirs, args[i])
+			continue
+		case "-D":
+			i++
+			defineMacro(defines, args[i])
+			continue
+		}
+		switch {
+		case strings.HasPrefix(arg, "-I"):
+			dirs = append(dirs, strings.TrimPrefix(arg, "-I"))
+		case strings.HasPrefix(arg, "--include-directory="):
+			dirs = append(dirs, strings.TrimPrefix(arg, "--include-directory="))
+		case strings.HasPrefix(arg, "-iquote"):
+			dirs = append(dirs, strings.TrimPrefix(arg, "-iquote"))
+		case strings.HasPrefix(arg, "-isystem"):
+			dirs = append(dirs, strings.TrimPrefix(arg, "-isystem"))
+		case strings.HasPrefix(arg, "--sysroot="):
+			sysroots = append(sysroots, strings.TrimPrefix(arg, "--sysroot="))
+		case strings.HasPrefix(arg, "-D"):
+			defineMacro(defines, strings.TrimPrefix(arg, "-D"))
+
+		case !strings.HasPrefix(arg, "-"):
+			ext := filepath.Ext(arg)
+			switch ext {
+			case ".c", ".cc", ".cxx", ".cpp", ".m", ".mm", ".S":
+				files = append(files, arg)
+			}
+		}
+	}
+	return files, dirs, sysroots, defines, nil
+}
+
+func defineMacro(defines map[string]string, arg string) {
+	// arg: macro=value
+	macro, value, ok := strings.Cut(arg, "=")
+	if !ok {
+		// just `-D MACRO`
+		return
+	}
+	if value == "" {
+		// `-D MACRO=`
+		// no value
+		return
+	}
+	switch value[0] {
+	case '<', '"':
+		// <path.h> or "path.h"?
+		defines[macro] = value
+	}
+}
---
+++ toolsupport/gccutil/scandepsparams_test.go
@@ -0,0 +1,109 @@
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package gccutil
+
+import (
+	"context"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestScanDepsParams(t *testing.T) {
+	ctx := context.Background()
+	type result struct {
+		Files    []string
+		Dirs     []string
+		Sysroots []string
+		Defines  map[string]string
+		Err      error
+	}
+	for _, tc := range []struct {
+		name string
+		args []string
+		env  []string
+		want result
+	}{
+		{
+			name: "clang++",
+			args: []string{
+				"../../third_party/llvm-build/Release+Asserts/bin/clang++",
+				"-MMD",
+				"-MF",
+				"obj/base/base/base64.o.d",
+				"-DDCHECK_ALWAYS_ON=1",
+				`-DCR_CLANG_REVISION="llvmorg-17-init-10134-g3da83fba-1"`,
+				"-I../..",
+				"-Igen",
+				"-D__DATE_=",
+				"-isystem../../buildtools/third_party/libc++/trunk/include",
+				"-isystem../../buildtools/third_party/libc++abi/trunk/include",
+				"--sysroot=../../build/linux/debian_bullseye_amd64-sysroot",
+				"-c",
+				"../../base/base64.cc",
+				"-o",
+				"obj/base/base/base64.o",
+			},
+			want: result{
+				Files: []string{
+					"../../base/base64.cc",
+				},
+				Dirs: []string{
+					"../..",
+					"gen",
+					"../../buildtools/third_party/libc++/trunk/include",
+					"../../buildtools/third_party/libc++abi/trunk/include",
+				},
+				Sysroots: []string{
+					"../../third_party/llvm-build/Release+Asserts",
+					"../../build/linux/debian_bullseye_amd64-sysroot",
+				},
+				Defines: map[string]string{
+					"CR_CLANG_REVISION": `"llvmorg-17-init-10134-g3da83fba-1"`,
+				},
+			},
+		},
+		{
+			name: "nacl-gcc",
+			args: []string{
+				"../../native_client/toolchain/linux_x86/nacl_x86_glibc/bin/x86_64-nacl-gcc",
+				"-MMD",
+				"-MF",
+				"obj/base/base/base64.o.d",
+				"-DDCHECK_ALWAYS_ON=1",
+				"-DNACL_TC_REV=73e5a44d837e54d335b4c618e1dd5d2028947a67",
+				"-I../..",
+				"-Igen",
+				"-D__DATE_=",
+				"-c",
+				"../../base/base64.cc",
+				"-o",
+				"obj/base/base/base64.o",
+			},
+			want: result{
+				Files: []string{
+					"../../base/base64.cc",
+				},
+				Dirs: []string{
+					"../..",
+					"gen",
+				},
+				Sysroots: []string{
+					"../../native_client/toolchain/linux_x86/nacl_x86_glibc",
+				},
+				Defines: map[string]string{},
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			var got result
+			got.Files, got.Dirs, got.Sysroots, got.Defines, got.Err = ScanDepsParams(ctx, tc.args, tc.env)
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Errorf("ScanDepsParams(ctx, %q, %q): diff -want +got:\n%s", tc.args, tc.env, diff)
+			}
+		})
+	}
+
+}
