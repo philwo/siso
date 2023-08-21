d850fd1bb87cdb154bbf3564e3251f02f96819ee
Fumitoshi Ukai <ukai@google.com>
2023-08-21 05:30:58 +0000

    siso: keepalive 30s
    Bug: b/286237547
    Change-Id: I10b4267a15dffb8f02a98f9a3eda34945da886f5
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4795792
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Commit-Queue: Richard Wang <richardwa@google.com>
    Reviewed-by: Richard Wang <richardwa@google.com>
    Cr-Commit-Position: refs/heads/main@{#59097}
    GitOrigin-RevId: 46220d8e59e32d69c6149241a876e5a938eb9f5a

--- reapi/reapi.go
+++ reapi/reapi.go
@@ -57,7 +57,8 @@ func (o *Option) RegisterFlags(fs *flag.FlagSet, envs map[string]string) {
 	fs.Int64Var(&o.CompressedBlob, "reapi_compress_blob", 1024, "use compressed blobs if server supports compressed blobs and size is bigger than this. specify 0 to disable comporession.")
 
 	// https://grpc.io/docs/guides/keepalive/#keepalive-configuration-specification
-	fs.DurationVar(&o.KeepAliveParams.Time, "reapi_grpc_keepalive_time", 2*time.Hour, "grpc keepalive time")
+	// b/286237547 - RBE suggests 30s
+	fs.DurationVar(&o.KeepAliveParams.Time, "reapi_grpc_keepalive_time", 30*time.Second, "grpc keepalive time")
 	fs.DurationVar(&o.KeepAliveParams.Timeout, "reapi_grpc_keepalive_timeout", 20*time.Second, "grpc keepalive timeout")
 	fs.BoolVar(&o.KeepAliveParams.PermitWithoutStream, "reapi_grpc_keepalive_permit_without_stream", false, "grpc keepalive permit without stream")
 }
