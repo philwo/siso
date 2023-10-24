3404b3f580c2f6dbeb0681e46502bb776da79383
Junji Watanabe <jwata@google.com>
2023-10-24 08:52:15 +0000

    Revert "Reland "[siso] Update hashfs with the output digests in Reproxy's RunResponse""
    This reverts commit d0e8ac4eea8653ec71eead9fef9fff40bce14d5d.
    
    Reason for revert: We want to prioritize a stable release.
    Will re-land this CL after next release.
    
    Original change's description:
    > Reland "[siso] Update hashfs with the output digests in Reproxy's RunResponse"
    >
    > This reverts commit cf09926ed05e8984978ade64a7c5fa9e74d6474c.
    >
    > Reason for revert: https://crrev.com/c/4930319 fixes the divergence
    > in input trees for compiles.
    >
    > Original change's description:
    > > Revert "[siso] Update hashfs with the output digests in Reproxy's RunResponse"
    > >
    > > This reverts commit 9c07cb1f7b5e0b120628b646a224f6ba376e46c5.
    > >
    > > Reason for revert: Suspect cache misses due to this change.
    > >
    > > Original change's description:
    > > > [siso] Update hashfs with the output digests in Reproxy's RunResponse
    > > >
    > > > Siso `cmd.HashFS.UpdateFromLocal()` reads output files and re-calculate digests. However, they aren't necessary because Reproxy returns the digests of the outputs.
    > > >
    > > > This CL skips the digest calculation when Reproxy completes a remote action successfully.
    > > >
    > > > Perf comparison on Linux p620: http://gpaste/5990543787032576
    > > > >   skip outputs digest calculation ran
    > > >     1.19 ± 0.22 times faster than before
    > > >
    > > > Bug: b/303163445, b/303551128
    > > > Change-Id: If0ceb8e0d2a5c4e2524c8de3ffcfbdb9e4f93a07
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4903418
    > > > Auto-Submit: Junji Watanabe <jwata@google.com>
    > > > Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    > > > Commit-Queue: Junji Watanabe <jwata@google.com>
    > > > Cr-Commit-Position: refs/heads/main@{#60208}
    > >
    > > Bug: b/303163445, b/303551128
    > > Change-Id: Ifd63e5fc0087bf4ef832cd05f2e6828b6193fe7b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4930749
    > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    > > Commit-Queue: Junji Watanabe <jwata@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#60324}
    >
    > Bug: b/303163445, b/303551128, b/304661271
    > Change-Id: I04b02e9a199c2a72fa4f9ee89f40f781a7b23a4f
    > Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4931128
    > Reviewed-by: Fumitoshi Ukai <ukai@google.com>
    > Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    > Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#60618}
    
    Bug: b/303163445, b/303551128, b/304661271
    Change-Id: I5d7540f92dcbb59d9bf0ee7401abb0df76bc0d4f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/4971472
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Junji Watanabe <jwata@google.com>
    Cr-Commit-Position: refs/heads/main@{#60619}
    GitOrigin-RevId: 5fd26315b00ee85bbdc298869c8288e9606c99a5

--- execute/reproxyexec/reproxyexec_test.go
+++ execute/reproxyexec/reproxyexec_test.go
@@ -10,42 +10,16 @@ import (
 	"path/filepath"
 	"testing"
 
-	cpb "github.com/bazelbuild/remote-apis-sdks/go/api/command"
-	"github.com/golang/glog"
-	"github.com/google/go-cmp/cmp"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
 	"infra/build/siso/execute"
 	"infra/build/siso/execute/reproxyexec/reproxytest"
 	"infra/build/siso/hashfs"
-	"infra/build/siso/o11y/iometrics"
-	"infra/build/siso/reapi/digest"
-	lpb "infra/third_party/reclient/api/log"
 	ppb "infra/third_party/reclient/api/proxy"
 )
 
-func setupDir(t *testing.T, root, dname string) {
-	t.Helper()
-	fullname := filepath.Join(root, dname)
-	err := os.MkdirAll(fullname, 0755)
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func setupFile(t *testing.T, dir, fname string) {
-	t.Helper()
-	setupDir(t, dir, filepath.Dir(fname))
-	fullname := filepath.Join(dir, fname)
-	err := os.WriteFile(fullname, nil, 0644)
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
 func TestRun_Unauthenticated(t *testing.T) {
-	defer glog.Flush()
 	ctx := context.Background()
 	dir := t.TempDir()
 
@@ -53,16 +27,28 @@ func TestRun_Unauthenticated(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer func() {
-		err := hashFS.Close(ctx)
+	defer hashFS.Close(ctx)
+
+	setupDir := func(dname string) {
+		t.Helper()
+		fullname := filepath.Join(dir, dname)
+		err := os.MkdirAll(fullname, 0755)
 		if err != nil {
 			t.Fatal(err)
 		}
-	}()
-
-	setupFile(t, dir, "base/base.cc")
-	setupFile(t, dir, "third_party/llvm-build/Release+Asserts/bin/clang++")
-	setupDir(t, dir, "out/siso/obj/base")
+	}
+	setupFile := func(fname string) {
+		t.Helper()
+		setupDir(filepath.Dir(fname))
+		fullname := filepath.Join(dir, fname)
+		err = os.WriteFile(fullname, nil, 0644)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	setupFile("base/base.cc")
+	setupFile("third_party/llvm-build/Release+Asserts/bin/clang++")
+	setupDir("out/siso/obj/base")
 
 	s := reproxytest.NewServer(ctx, t, reproxytest.Fake{
 		RunCommandFunc: func(ctx context.Context, req *ppb.RunRequest) (*ppb.RunResponse, error) {
@@ -97,188 +83,5 @@ func TestRun_Unauthenticated(t *testing.T) {
 	})
 	if status.Code(err) != codes.Unauthenticated {
 		t.Errorf("Run(ctx, cmd)=%v; want %v", err, codes.Unauthenticated)
-	}
-}
-
-func TestRun_RemoteSuccess(t *testing.T) {
-	defer glog.Flush()
-	ctx := context.Background()
-	dir := t.TempDir()
-
-	hashFS, err := hashfs.New(ctx, hashfs.Option{})
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		err := hashFS.Close(ctx)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}()
-
-	setupFile(t, dir, "base/base.cc")
-	setupFile(t, dir, "third_party/llvm-build/Release+Asserts/bin/clang++")
-	setupDir(t, dir, "out/siso/obj/base")
-
-	testOutDigest := digest.FromBytes("", []byte("fake data")).Digest()
-	s := reproxytest.NewServer(ctx, t, reproxytest.Fake{
-		RunCommandFunc: func(ctx context.Context, req *ppb.RunRequest) (*ppb.RunResponse, error) {
-			err := os.WriteFile(filepath.Join(req.Command.ExecRoot, req.Command.GetOutput().GetOutputFiles()[0]), nil, 0644)
-			if err != nil {
-				t.Fatalf("failed to write output files. %v", err)
-			}
-			return &ppb.RunResponse{
-				Result: &cpb.CommandResult{
-					Status: cpb.CommandResultStatus_SUCCESS,
-				},
-				ActionLog: &lpb.LogRecord{
-					CompletionStatus: lpb.CompletionStatus_STATUS_REMOTE_EXECUTION,
-					RemoteMetadata: &lpb.RemoteMetadata{
-						Result: &cpb.CommandResult{
-							Status: cpb.CommandResultStatus_SUCCESS,
-						},
-						OutputFileDigests: map[string]string{
-							"obj/base/base.o": testOutDigest.String(),
-						},
-					},
-				},
-			}, nil
-		},
-	})
-	t.Cleanup(s.Close)
-	re := REProxyExec{
-		connAddress: s.Addr(),
-	}
-
-	err = re.Run(ctx, &execute.Cmd{
-		ID:       "stepid",
-		ExecRoot: dir,
-		Dir:      "out/siso",
-		HashFS:   hashFS,
-		Args:     []string{"../../third_party/llvm-build/Release+Asserts/bin/clang++", "-c", "../../base/base.cc", "-o", "obj/base/base.o"},
-		Inputs:   []string{"base/base.cc", "third_party/llvm-build/Release+Asserts/bin/clang++"},
-		Outputs:  []string{"out/siso/obj/base/base.o"},
-		REProxyConfig: &execute.REProxyConfig{
-			ExecStrategy: "remote_local_fallback",
-			Labels: map[string]string{
-				"type":     "compile",
-				"compiler": "clang",
-				"lang":     "cpp",
-			},
-			Platform: map[string]string{
-				"container-image": "gcr.io/chops-public-images-prod/xxx@sha256:xxx",
-				"OSFamily":        "Linux",
-			},
-		},
-	})
-	if err != nil {
-		t.Errorf("Run(ctx, cmd)=%v; want nil", err)
-	}
-
-	// No IO operations should be taken by HashFS.
-	wantStats := iometrics.Stats{}
-	if diff := cmp.Diff(wantStats, hashFS.IOMetrics.Stats()); diff != "" {
-		t.Errorf("hashFS.IOMetrics.Stats(): diff -want +got:\n%s", diff)
-	}
-
-	// Check if the output digests are taken from RunResponse.
-	entries, err := hashFS.Entries(ctx, dir, []string{"out/siso/obj/base/base.o"})
-	if err != nil {
-		t.Errorf("hashFS.Entries()=%v; want nil", err)
-	}
-	gotDg := entries[0].Data.Digest().String()
-	wantDg := testOutDigest.String()
-	if gotDg != wantDg {
-		t.Errorf("output digest: got %s; want %s", gotDg, wantDg)
-	}
-}
-
-func TestRun_LocalFallback(t *testing.T) {
-	defer glog.Flush()
-	ctx := context.Background()
-	dir := t.TempDir()
-
-	hashFS, err := hashfs.New(ctx, hashfs.Option{})
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer func() {
-		err := hashFS.Close(ctx)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}()
-
-	setupFile(t, dir, "base/base.cc")
-	setupFile(t, dir, "third_party/llvm-build/Release+Asserts/bin/clang++")
-	setupDir(t, dir, "out/siso/obj/base")
-
-	testOutput := []byte("local fallback!")
-	s := reproxytest.NewServer(ctx, t, reproxytest.Fake{
-		RunCommandFunc: func(ctx context.Context, req *ppb.RunRequest) (*ppb.RunResponse, error) {
-			err := os.WriteFile(filepath.Join(req.Command.ExecRoot, req.Command.GetOutput().GetOutputFiles()[0]), testOutput, 0644)
-			if err != nil {
-				t.Fatalf("failed to write output files. %v", err)
-			}
-			return &ppb.RunResponse{
-				Result: &cpb.CommandResult{
-					Status: cpb.CommandResultStatus_SUCCESS,
-				},
-				ActionLog: &lpb.LogRecord{
-					CompletionStatus: lpb.CompletionStatus_STATUS_LOCAL_FALLBACK,
-				},
-			}, nil
-		},
-	})
-	t.Cleanup(s.Close)
-	re := REProxyExec{
-		connAddress: s.Addr(),
-	}
-
-	err = re.Run(ctx, &execute.Cmd{
-		ID:       "stepid",
-		ExecRoot: dir,
-		Dir:      "out/siso",
-		HashFS:   hashFS,
-		Args:     []string{"../../third_party/llvm-build/Release+Asserts/bin/clang++", "-c", "../../base/base.cc", "-o", "obj/base/base.o"},
-		Inputs:   []string{"base/base.cc", "third_party/llvm-build/Release+Asserts/bin/clang++"},
-		Outputs:  []string{"out/siso/obj/base/base.o"},
-		REProxyConfig: &execute.REProxyConfig{
-			ExecStrategy: "remote_local_fallback",
-			Labels: map[string]string{
-				"type":     "compile",
-				"compiler": "clang",
-				"lang":     "cpp",
-			},
-			Platform: map[string]string{
-				"container-image": "gcr.io/chops-public-images-prod/xxx@sha256:xxx",
-				"OSFamily":        "Linux",
-			},
-		},
-	})
-	if err != nil {
-		t.Errorf("Run(ctx, cmd)=%v; want nil", err)
-	}
-
-	// HashFS reads out/siso/obj/base/base.o to calculate the digest.
-	got := hashFS.IOMetrics.Stats()
-	want := iometrics.Stats{
-		Ops:    2,
-		ROps:   1,
-		RBytes: int64(len(testOutput)),
-	}
-	if diff := cmp.Diff(want, got); diff != "" {
-		t.Errorf("hashFS.IOMetrics.Stats(): diff -want +got:\n%s", diff)
-	}
-
-	// Check the output digests.
-	entries, err := hashFS.Entries(ctx, dir, []string{"out/siso/obj/base/base.o"})
-	if err != nil {
-		t.Errorf("hashFS.Entries()=%v; want nil", err)
-	}
-	gotDg := entries[0].Data.Digest().String()
-	wantDg := digest.FromBytes("", testOutput).Digest().String()
-	if gotDg != wantDg {
-		t.Errorf("output digest: got %s; want %s", gotDg, wantDg)
 	}
 }
