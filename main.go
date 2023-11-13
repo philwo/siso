a3a996c4eac8fb875e9206f90d490c39478e9660
Fumitoshi Ukai <ukai@google.com>
2023-11-13 03:52:33 +0000

    siso: update version string to v0.0.21
    v0.0.20 has been rolled out as pinned version at
    git_revision:69aa430ab034fa88d609bd22cf018c1367d49ea2
    https://chromium-review.googlesource.com/c/chromium/src/+/5020113
    
    Bug: b/309569058
    Change-Id: I2d7f21195f4184b3cdbbe39805af544ffc65da18
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5020267
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Cr-Commit-Position: refs/heads/main@{#61132}
    GitOrigin-RevId: da4f9b27da012e58a3528c124e313400efa9747d

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ var (
 	traceFile     string
 )
 
-const versionStr = "siso v0.0.20"
+const versionStr = "siso v0.0.21"
 
 func getApplication(authOpts cred.Options) *cli.Application {
 	return &cli.Application{
