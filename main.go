104d77eb9d66054aa4e90a191c716dc4883107c9
Fumitoshi Ukai <ukai@google.com>
2024-03-12 04:17:19 +0000

    siso: update version string to v0.0.36
    v0.0.35 has been rolled out as pinned version at
    git_revision:110b1d8c0528de153cef259f09f3dc5ee627e6cb
    https://chromium-review.googlesource.com/c/chromium/src/+/5364536
    
    Bug: b/328319354
    Change-Id: I2e1c6e9fb8a183798a9263edce8b6a8ef1d19b69
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5364476
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Cr-Commit-Position: refs/heads/main@{#63715}
    GitOrigin-RevId: 7c153115fa970f093ee2adc3329ef75aa08cde55

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ var (
 	traceFile     string
 )
 
-const versionStr = "siso v0.0.35"
+const versionStr = "siso v0.0.36"
 
 func getApplication(authOpts cred.Options) *cli.Application {
 	return &cli.Application{
