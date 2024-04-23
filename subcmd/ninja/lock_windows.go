9957309d4b0961bcf553ade41fb86b264b32cd37
Fumitoshi Ukai <ukai@google.com>
2024-04-23 07:09:25 +0000

    siso: lock on windows
    during other build running in the same out dir,
    siso fails with
     0.00s Error: .siso_lock is locked by pid=31532: The process cannot access the file because another process has locked a portion of the file.
    
    Bug: b/326003101
    Change-Id: I5dfc6a8e43dc9f1699f93a10c54f63ef5a730839
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5474677
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Reviewed-by: Philipp Wollermann <philwo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#64722}
    GitOrigin-RevId: 3512ef9868d34004eac5e06fb711d4b2eb5b15a2

--- subcmd/ninja/lock_windows.go
+++ subcmd/ninja/lock_windows.go
@@ -9,16 +9,59 @@ package ninja
 import (
 	"context"
 	"errors"
+	"fmt"
+	"math"
+	"os"
+
+	"golang.org/x/sys/windows"
 )
 
-// TODO: support lock file on windows
-
-type lockFile struct{}
-
-func newLockFile(ctx context.Context, fname string) (*lockFile, error) {
-	return nil, errors.ErrUnsupported
+type lockFile struct {
+	f       *os.File
+	pidfile string
 }
 
-func (l *lockFile) Close() error  { return errors.ErrUnsupported }
-func (l *lockFile) Lock() error   { return errors.ErrUnsupported }
-func (l *lockFile) Unlock() error { return errors.ErrUnsupported }
+func newLockFile(ctx context.Context, fname string) (*lockFile, error) {
+	f, err := os.OpenFile(fname, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, err
+	}
+	return &lockFile{f: f, pidfile: fname + ".pid"}, nil
+}
+
+func (l *lockFile) Close() error {
+	return l.f.Close()
+}
+
+func (l *lockFile) Lock() error {
+	const reserved = 0
+	const lowByteRange = math.MaxUint32
+	const highByteRange = math.MaxUint32
+	err := windows.LockFileEx(
+		windows.Handle(l.f.Fd()),
+		windows.LOCKFILE_EXCLUSIVE_LOCK|windows.LOCKFILE_FAIL_IMMEDIATELY,
+		reserved, lowByteRange, highByteRange,
+		&windows.Overlapped{})
+	if err != nil {
+		if errors.Is(err, windows.ERROR_LOCK_VIOLATION) {
+			// can't read lockfile when locked.
+			buf, err := os.ReadFile(l.pidfile)
+			if err != nil {
+				return fmt.Errorf("%s is locked, and failed to read %s: %w", l.f.Name(), l.pidfile, err)
+			}
+			return fmt.Errorf("%s is locked by %s: %w", l.f.Name(), string(buf), windows.ERROR_LOCK_VIOLATION)
+		}
+		return err
+	}
+	return os.WriteFile(l.pidfile, []byte(fmt.Sprintf("pid=%d", os.Getpid())), 0644)
+}
+
+func (l *lockFile) Unlock() error {
+	const reserved = 0
+	const lowByteRange = math.MaxUint32
+	const highByteRange = math.MaxUint32
+	return windows.UnlockFileEx(
+		windows.Handle(l.f.Fd()),
+		reserved, lowByteRange, highByteRange,
+		&windows.Overlapped{})
+}
