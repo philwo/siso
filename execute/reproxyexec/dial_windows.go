6f754c999a97edf609d86ea6d6b8bf8e4501318f
Fumitoshi Ukai <ukai@google.com>
2024-06-24 03:25:12 +0000

    siso: use grpc.NewClient in reproxyexec without block
    grpc.DialContext is deprecated.
    but reverted for reproxyexec, because blocking code didn't work well.
    but rewrapper doesn't use WithBlock,
    so siso doesn't need WithBlock too.
    
    Change-Id: I7b1888c44a3afc27b2b4344d75b736825b68c758
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5647265
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Philipp Wollermann <philwo@google.com>
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#66360}
    GitOrigin-RevId: 35aeb5b807513207b9ac38befda8d5703171cd9c

--- execute/reproxyexec/dial_windows.go
+++ execute/reproxyexec/dial_windows.go
@@ -15,33 +15,23 @@ import (
 	"google.golang.org/grpc"
 )
 
-// DialContext connects to the serverAddress for grpc.
+// dialContext connects to the serverAddress for grpc.
 // if serverAddr is `pipe://<addr>`, it connects to named pipe (`\\.\\pipe\<addr>`).
-func DialContext(ctx context.Context, serverAddr string) (*grpc.ClientConn, error) {
+func dialContext(ctx context.Context, serverAddr string) (*grpc.ClientConn, error) {
 	if strings.HasPrefix(serverAddr, "pipe://") {
 		return dialPipe(ctx, strings.TrimPrefix(serverAddr, "pipe://"))
 	}
-	// Although grpc.DialContxt is deprecated, but grpc.NewClient
-	// doesn't have blocking feature.
-	// https://crrev.com/c/5642324 didn't work well?
-	// https://ci.chromium.org/ui/p/chromium/builders/build/win-build-perf-developer/1232/overview
-	return grpc.DialContext(
-		ctx,
+	return grpc.NewClient(
 		serverAddr,
 		grpc.WithInsecure(),
-		// Ensure blocking due to flaky reproxy behavior http://tg/639661.
-		grpc.WithBlock(),
 		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(grpcMaxMsgSize)))
 }
 
 func dialPipe(ctx context.Context, pipe string) (*grpc.ClientConn, error) {
 	addr := `\\.\pipe\` + pipe
-	return grpc.DialContext(
-		ctx,
+	return grpc.NewClient(
 		addr,
 		grpc.WithInsecure(),
-		// Ensure blocking due to flaky reproxy behavior http://tg/639661.
-		grpc.WithBlock(),
 		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(grpcMaxMsgSize)),
 		grpc.WithContextDialer(func(ctx context.Context, _ string) (net.Conn, error) {
 			return winio.DialPipeContext(ctx, addr)
