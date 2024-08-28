dcd6bfab361ee00da80f06bf9da7604acfff3c9e
Fumitoshi Ukai <ukai@google.com>
2024-08-28 03:15:41 +0000

    siso: remove remote exec retry logic for no-fallback
    Remote exec retry logic was added to mitigate DeadlineExceeded error
    due to docker image pull.
    
    We consolidated platform container image variety, so most active
    platform container images are prefetched.
    We don't observe DeadlineExceeded error recently, so no need to
    this retry logic.
    
    Bug: b/353878534
    Change-Id: I70dbad69393edf5f222ad4d491a67a429e0420ff
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5820072
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Cr-Commit-Position: refs/heads/main@{#67949}
    GitOrigin-RevId: dc70c596255ee95fcce9c07a82f4782e85bbd297

--- build/exec_remote.go
+++ build/exec_remote.go
@@ -10,70 +10,49 @@ import (
 	"time"
 
 	rpb "github.com/bazelbuild/remote-apis/build/bazel/remote/execution/v2"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 
 	"infra/build/siso/o11y/clog"
 	"infra/build/siso/o11y/trace"
 	"infra/build/siso/reapi"
-	"infra/build/siso/reapi/retry"
 )
 
 func (b *Builder) execRemote(ctx context.Context, step *Step) error {
 	ctx, span := trace.NewSpan(ctx, "exec-remote")
 	defer span.Close(nil)
-	noFallback := experiments.Enabled("no-fallback", "no-fallback tries for reapi deadline exceeded at most 4 times")
-	var timeout time.Duration
-	if noFallback {
-		// In no-fallback mode, remote execution will be tried 4 times at most.
-		// Since an execution sets cmd.Timeout * 2 as action timeout, the 2nd try might be deduplicated by RBE scheduler. 3rd or 4th try should successfully restart a new execution.
-		// Alternatively, Siso could avoid extending action timeout in no-fallback mode.
-		// However, this will lose the opportunity to cache long actions.
-		timeout = step.cmd.Timeout * 4
-	}
 	clog.Infof(ctx, "exec remote %s", step.cmd.Desc)
-	var reExecDur time.Duration
-	err := retry.Do(ctx, func() error {
-		err := b.remoteSema.Do(ctx, func(ctx context.Context) error {
-			reExecStarted := time.Now()
-			step.metrics.ActionStartTime = IntervalMetric(reExecStarted.Sub(b.start))
-			ctx = reapi.NewContext(ctx, &rpb.RequestMetadata{
-				ActionId:                step.cmd.ID,
-				ToolInvocationId:        b.id,
-				CorrelatedInvocationsId: b.jobID,
-				ActionMnemonic:          step.def.ActionName(),
-				TargetId:                step.cmd.Outputs[0],
-			})
-			clog.Infof(ctx, "step state: remote exec")
+	err := b.remoteSema.Do(ctx, func(ctx context.Context) error {
+		reExecStarted := time.Now()
+		step.metrics.ActionStartTime = IntervalMetric(reExecStarted.Sub(b.start))
+		ctx = reapi.NewContext(ctx, &rpb.RequestMetadata{
+			ActionId:                step.cmd.ID,
+			ToolInvocationId:        b.id,
+			CorrelatedInvocationsId: b.jobID,
+			ActionMnemonic:          step.def.ActionName(),
+			TargetId:                step.cmd.Outputs[0],
+		})
+		clog.Infof(ctx, "step state: remote exec")
+		step.setPhase(stepRemoteRun)
+		err := b.remoteExec.Run(ctx, step.cmd)
+		step.setPhase(stepOutput)
+		step.metrics.IsRemote = true
+		result, cached := step.cmd.ActionResult()
+		if err == nil && !validateRemoteActionResult(result) {
+			clog.Errorf(ctx, "no outputs in action result. retry without cache lookup. b/350360391")
+			step.cmd.SkipCacheLookup = true
 			step.setPhase(stepRemoteRun)
-			err := b.remoteExec.Run(ctx, step.cmd)
+			err = b.remoteExec.Run(ctx, step.cmd)
 			step.setPhase(stepOutput)
 			step.metrics.IsRemote = true
-			result, cached := step.cmd.ActionResult()
+			result, cached = step.cmd.ActionResult()
 			if err == nil && !validateRemoteActionResult(result) {
-				clog.Errorf(ctx, "no outputs in action result. retry without cache lookup. b/350360391")
-				step.cmd.SkipCacheLookup = true
-				step.setPhase(stepRemoteRun)
-				err = b.remoteExec.Run(ctx, step.cmd)
-				step.setPhase(stepOutput)
-				step.metrics.IsRemote = true
-				result, cached = step.cmd.ActionResult()
-				if err == nil && !validateRemoteActionResult(result) {
-					clog.Errorf(ctx, "no outputs in action result again. b/350360391")
-				}
+				clog.Errorf(ctx, "no outputs in action result again. b/350360391")
 			}
-			if cached {
-				step.metrics.Cached = true
-			}
-			step.metrics.RunTime = IntervalMetric(time.Since(reExecStarted))
-			step.metrics.done(ctx, step)
-			return err
-		})
-		reExecDur += time.Duration(step.metrics.RunTime)
-		if code := status.Code(err); noFallback && (code == codes.DeadlineExceeded || errors.Is(err, context.DeadlineExceeded)) && reExecDur < timeout {
-			clog.Warningf(ctx, "exec remote timedout duration=%s timeout=%s: %v", reExecDur, timeout, err)
-			err = status.Errorf(codes.Unavailable, "reapi timedout %v", err)
 		}
+		if cached {
+			step.metrics.Cached = true
+		}
+		step.metrics.RunTime = IntervalMetric(time.Since(reExecStarted))
+		step.metrics.done(ctx, step)
 		return err
 	})
 	if err != nil {
