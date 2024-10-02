bc27c585ba6c8f1c26a5c0d95344b58a5dfa4270
Fumitoshi Ukai <ukai@google.com>
2024-10-02 06:45:36 +0000

    siso: extract rule ralated code in rule.go
    prepare for https://chromium-review.googlesource.com/c/infra/infra/+/5876000
    
    Bug: b/365906703
    Change-Id: Ie8e9530097ae8f1c295c277fa856ced5459ca8dd
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5903517
    Commit-Queue: Richard Wang <richardwa@google.com>
    Reviewed-by: Richard Wang <richardwa@google.com>
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Cr-Commit-Position: refs/heads/main@{#68852}
    GitOrigin-RevId: cf25b9d12dd54283ee0ed5bd7072159d3052875d

--- toolsupport/ninjautil/eval_env.go
+++ toolsupport/ninjautil/eval_env.go
@@ -132,40 +132,6 @@ func (e EvalString) String() string {
 	return sb.String()
 }
 
-// rule represents a build rule.
-// Further reading: https://ninja-build.org/manual.html#_rules
-// TODO(b/267409605): Add tests for Rule methods.
-type rule struct {
-	name     string
-	bindings map[string]EvalString
-}
-
-func newRule(name string) *rule {
-	return &rule{
-		name:     name,
-		bindings: make(map[string]EvalString),
-	}
-}
-
-// Name returns rule's name.
-func (r *rule) Name() string { return r.name }
-
-// addBinding adds bindings to the rule.
-func (r *rule) addBinding(key string, val EvalString) {
-	r.bindings[key] = val
-}
-
-// Binding returns binding in the rule.
-func (r *rule) Binding(key string) (EvalString, bool) {
-	v, ok := r.bindings[key]
-	return v, ok
-}
-
-func (r *rule) hasBinding(key string) bool {
-	_, ok := r.bindings[key]
-	return ok
-}
-
 // BindingEnv is an implementation of the Env interface which holds mappings of
 // binding names to values, and a pointer to a parent BindingEnv.
 // TODO(b/267409605): Add tests for BindingEnv methods.
@@ -231,39 +197,4 @@ func (b *BindingEnv) Print(w io.Writer) {
 		fmt.Fprintf(w, "%s = %s\n", escapeNinjaToken(k), escapeNinjaValue(b.bindings[k]))
 	}
 	fmt.Fprintln(w)
-}
-
-// ruleBinding is a mappings of rule names to rules.
-type ruleBinding struct {
-	rules  map[string]*rule
-	parent *ruleBinding
-}
-
-func newRuleBinding(parent *ruleBinding) *ruleBinding {
-	return &ruleBinding{
-		rules:  make(map[string]*rule),
-		parent: parent,
-	}
-}
-
-func (b *ruleBinding) addRule(rule *rule) {
-	b.rules[rule.Name()] = rule
-}
-
-// lookupRule looks up rules in the binding env.
-func (b *ruleBinding) lookupRule(ruleName string) (*rule, bool) {
-	r, ok := b.rules[ruleName]
-	if ok {
-		return r, true
-	}
-	if b.parent != nil {
-		return b.parent.lookupRule(ruleName)
-	}
-	return nil, false
-}
-
-// lookupRuleCurrentScope looks up rules in the current scope in the binding env.
-func (b *ruleBinding) lookupRuleCurrentScope(ruleName string) (*rule, bool) {
-	r, ok := b.rules[ruleName]
-	return r, ok
 }
