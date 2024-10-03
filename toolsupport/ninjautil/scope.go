c4f2d26d6342aae5b613f5aea09c3e2236333ed4
Fumitoshi Ukai <ukai@google.com>
2024-10-03 06:28:40 +0000

    siso: add fileScope and buildScope - for new ninja parser
    extract from https://chromium-review.googlesource.com/c/infra/infra/+/5876000
    
    Bug: b/365906703
    Change-Id: I76c1aa9645c60701606314840f6bfa1185ff9deb
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5904561
    Reviewed-by: Philipp Wollermann <philwo@google.com>
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Cr-Commit-Position: refs/heads/main@{#68895}
    GitOrigin-RevId: fe804f390401d903679a3a9d08814bd79d0f0256

---
+++ toolsupport/ninjautil/scope.go
@@ -0,0 +1,123 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package ninjautil
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"sort"
+)
+
+// fileScope is ninja rule/binding scope (in a file).
+type fileScope struct {
+	rules    *ruleMap
+	bindings *shardBindings
+	parent   *fileScope
+}
+
+// newFileScope creates new fileScope.
+func newFileScope(parent *fileScope) *fileScope {
+	return &fileScope{
+		parent: parent,
+	}
+}
+
+// lookupRule looks up rule by name in the scope and its parents, if any.
+func (s *fileScope) lookupRule(ruleName []byte) (*rule, bool) {
+	r, ok := s.rules.lookupRule(ruleName)
+	if ok {
+		return r, true
+	}
+	if s.parent != nil {
+		return s.parent.lookupRule(ruleName)
+	}
+	return nil, false
+}
+
+// setRule sets rule in the scope.
+func (s *fileScope) setRule(rule *rule) error {
+	return s.rules.setRule(rule)
+}
+
+// lookupVar looks up variable binding in the scope and its parents, if any.
+func (s *fileScope) lookupVar(pos int, name []byte) (evalString, bool) {
+	v, ok := s.bindings.get(pos, name)
+	if ok {
+		return v, ok
+	}
+	if s.parent != nil {
+		return s.parent.lookupVar(-1, name)
+	}
+	return evalString{}, false
+}
+
+// setVar sets variable "name=value".
+func (s *fileScope) setVar(name []byte, value evalString) {
+	s.bindings.set(name, value)
+}
+
+// Print prints scope in w.
+func (s *fileScope) Print(w io.Writer) {
+	if s == nil {
+		return
+	}
+	if s.parent != nil {
+		s.parent.Print(w)
+	}
+	keys := s.bindings.keys()
+	if len(keys) == 0 {
+		return
+	}
+	sort.Strings(keys)
+	for _, key := range keys {
+		ev, _ := s.bindings.get(0, []byte(key))
+		fmt.Fprintf(w, "%s = %s\n", key, ev.v)
+	}
+	fmt.Fprintln(w)
+}
+
+// buildScope is a binding scope for a build.
+type buildScope struct {
+	buf        []byte
+	statements []statement
+}
+
+// lookupVar looks up var in a build statement, if any.
+func (b *buildScope) lookupVar(pos int, name []byte) (evalString, bool) {
+	for i := len(b.statements) - 1; i >= 0; i-- {
+		st := b.statements[i]
+		if pos >= 0 && st.pos >= pos {
+			continue
+		}
+		key := bytes.TrimSpace(b.buf[st.s:st.v])
+		if !bytes.Equal(key, name) {
+			continue
+		}
+		v := b.buf[st.v+1 : st.e]
+		v = bytes.TrimSpace(v)
+		return evalString{
+			v:   v,
+			esc: bytes.Count(v, []byte{'$'}),
+		}, true
+	}
+	return evalString{}, false
+}
+
+// set sets build bindings.
+func (b *buildScope) set(buf []byte, statements []statement) {
+	b.buf = buf
+	b.statements = statements
+}
+
+// Print prints bindings in w.
+func (b *buildScope) Print(w io.Writer) {
+	if len(b.statements) == 0 {
+		return
+	}
+	for _, st := range b.statements {
+		fmt.Fprintf(w, "%s", b.buf[st.s:st.e])
+	}
+}
