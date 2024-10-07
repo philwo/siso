1445d042e877d03ede6f603c87136b033a6536e8
Fumitoshi Ukai <ukai@google.com>
2024-10-07 02:13:18 +0000

    siso: update version string to v1.0.15
    v1.0.14 has been rolled out as pinned version at
    git_revision:6b2665e870db2df4da1c184a2aec2f98dcb75000
    https://chromium-review.googlesource.com/c/chromium/src/+/5905671
    
    Bug: b/370370961
    Change-Id: I2e420b59b7712177b375241d25e0035a23420be2
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/5909932
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Cr-Commit-Position: refs/heads/main@{#68978}
    GitOrigin-RevId: d6608b4d400ed27ad201ba4f9a0421eb455f80f2

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ var (
 	traceFile     string
 )
 
-const versionID = "v1.0.14"
+const versionID = "v1.0.15"
 const versionStr = "siso " + versionID
 
 func getApplication(authOpts cred.Options) *cli.Application {
