72b9b90a0f6b5ea16c6b9768ad6a660d4e21eb15
Fumitoshi Ukai <ukai@google.com>
2024-11-20 01:14:37 +0000

    siso: update version string to v1.0.24
    v1.0.23 has been rolled out as pinned version at
    git_revision:507984579a3b2e8fa59c1dde996a0901b62b8d7c
    https://chromium-review.googlesource.com/c/chromium/src/+/6034658o
    
    Bug: b/379562646
    Change-Id: I84fb8bd22f9e917056cec8f6cf5f55063260b649
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/6034151
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Cr-Commit-Position: refs/heads/main@{#70037}
    GitOrigin-RevId: 4940a14abd02fb15183cdb8bb05789de1980dfd1

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ var (
 	traceFile     string
 )
 
-const versionID = "v1.0.23"
+const versionID = "v1.0.24"
 const versionStr = "siso " + versionID
 
 func getApplication(authOpts cred.Options) *cli.Application {
