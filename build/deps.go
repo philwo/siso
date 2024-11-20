122975001f51ac8ca50c38f3540fb1983475b74d
Fumitoshi Ukai <ukai@google.com>
2024-11-20 06:48:32 +0000

    siso: disable fast-deps by default.
    To use fast-deps, set SISO_EXPERIMENTS=fast-deps
    
    Change-Id: I1c050e5fd714684348b4d8e1b2bee41d6bf73d02
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/6034153
    Reviewed-by: Philipp Wollermann <philwo@google.com>
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Cr-Commit-Position: refs/heads/main@{#70048}
    GitOrigin-RevId: 329538e155e2789e753da2825c91f2b2c1f3811c

--- build/deps.go
+++ build/deps.go
@@ -55,8 +55,8 @@ func depsFastStep(ctx context.Context, b *Builder, step *Step) (*Step, error) {
 	if b.hashFS.OnCog() {
 		return nil, errors.New("no fast-deps (on Cog)")
 	}
-	if experiments.Enabled("no-fast-deps", "disable fast-deps and force scandeps") {
-		return nil, errors.New("no fast-deps (SISO_EXPERIMENTS)")
+	if !experiments.Enabled("fast-deps", "") {
+		return nil, errors.New("no fast-deps (no SISO_EXPERIMENTS=fast-deps)")
 	}
 	ds, found := depsProcessors[step.cmd.Deps]
 	if !found {
