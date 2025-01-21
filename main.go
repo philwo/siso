043693d9b92697bbc2b04fe185a1943ce6ac848f
Junji Watanabe <jwata@google.com>
2025-01-20 22:41:26 -0800

    siso: update version string to v1.1.8
    v1.1.7 is being rolled out by https://crrev.com/c/6186603
    
    Bug: 391028804
    Change-Id: I1ca8e5ac619d68f283f8299f37b7e114c675af38
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/6186525
    Reviewed-by: Fumitoshi Ukai <ukai@google.com>
    Auto-Submit: Junji Watanabe <jwata@google.com>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Cr-Commit-Position: refs/heads/main@{#71060}
    GitOrigin-RevId: 6acc27cdcb688f622a0033e6efb73e28349cb9a0

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ var (
 	traceFile     string
 )
 
-const versionID = "v1.1.7"
+const versionID = "v1.1.8"
 const versionStr = "siso " + versionID
 
 func getApplication(authOpts cred.Options) *cli.Application {
