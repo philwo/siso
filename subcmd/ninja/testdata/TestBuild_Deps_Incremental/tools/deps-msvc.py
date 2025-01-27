40a08180b1598905ad54729a26a4f1d1f18710f0
Fumitoshi Ukai <ukai@google.com>
2025-01-27 00:17:01 -0800

    siso: add regression test for b/391971458
    add test to make sure deps log is not broken
    after no-update step.
    
    Bug: b/391971458
    Change-Id: Ia9252aed0fbfb33fa674b06ede6609daf9bb2dd9
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/6197683
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Cr-Commit-Position: refs/heads/main@{#71162}
    GitOrigin-RevId: 0b6159c39573013a79a47f8544200df07a6a74fc

---
+++ subcmd/ninja/testdata/TestBuild_Deps_Incremental/tools/deps-msvc.py
@@ -0,0 +1,20 @@
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import argparse
+import sys
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-o", type=argparse.FileType(mode='w'))
+  parser.add_argument("-c", type=argparse.FileType())
+  options = parser.parse_args()
+
+  options.o.write(options.c.read())
+  sys.stdout.write('Note: including file: ../../base/foo.h\n')
+
+
+if __name__ == "__main__":
+  sys.exit(main())
