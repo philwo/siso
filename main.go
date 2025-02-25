808b1bf0871c821cb065e265602e23bbbc6589c9
Junji Watanabe <jwata@google.com>
2025-02-24 18:18:35 -0800

    siso: Update the version string to 1.1.17
    v1.1.16 has been rolled out to chromium. https://crrev.com/c/6298559
    
    Bug: 397532338
    Change-Id: If15d388c5dc1f88f5aeb4a9d6991f3b6652e72b0
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/6300896
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#71819}
    GitOrigin-RevId: 5bfed3807cae4b42307f2d10c965730f0fa24a5d

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ var (
 	traceFile     string
 )
 
-const versionID = "v1.1.16"
+const versionID = "v1.1.17"
 const versionStr = "siso " + versionID
 
 func getApplication(authOpts cred.Options) *cli.Application {
