84c3cf6c830c2597e02adaa6a7cfcb3735ee9f15
Philipp Wollermann <philwo@google.com>
2025-03-22 07:27:48 +0900

    Remove tests

--- subcmd/ninja/testdata/TestBuild_KeepGoing/success.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2023 The Chromium Authors
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-import sys
-with open(sys.argv[1], "w") as f:
-  f.write("")
