d76bcd937bdf207c164b62eb67073c6ea3b8e675
Philipp Wollermann <philwo@google.com>
2025-03-22 09:24:58 +0900

    Don't crash when using -logtostderr

--- subcmd/ninja/ninja.go
+++ subcmd/ninja/ninja.go
@@ -1008,7 +1008,11 @@ func (c *ninjaCmdRun) initLogDir(ctx context.Context) error {
 	if err != nil {
 		return err
 	}
-	return c.logSymlink(ctx)
+	err = c.logSymlink(ctx)
+	if err != nil {
+		glog.Warningf("failed to create symlink for log: %v", err)
+	}
+	return nil
 }
 
 func (c *ninjaCmdRun) initFlags(targets []string) map[string]string {
