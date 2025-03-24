d88914b15253770a36f8efedc6967e0f2bc6f7c5
Fumitoshi Ukai <ukai@google.com>
2025-03-23 18:10:44 -0700

    siso: update version string to v1.1.22
    v1.1.21 has been rolled out as pinned version at
    git_revision:ae99e5f46d1fbc718ad9c28bb82c16883d7de99b
    https://chromium-review.googlesource.com/c/chromium/src/+/6383236
    
    Bug: b/404070843
    Change-Id: I195b92a6af70f2c38b7d4b521847c6f7d194f4c6
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/6385099
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Cr-Commit-Position: refs/heads/main@{#72414}
    GitOrigin-RevId: ce2d7528773372e6337d7459654ed26682246e45

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ var (
 	traceFile     string
 )
 
-const versionID = "v1.1.21"
+const versionID = "v1.1.22"
 const versionStr = "siso " + versionID
 
 func getApplication(authOpts cred.Options) *cli.Application {
