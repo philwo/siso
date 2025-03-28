512c674683607bb877cffb8f3f0dbcd38ad1d7d5
Philipp Wollermann <philwo@google.com>
2025-03-29 06:29:59 +0900

    Remove scanDepsSema

--- build/builder.go
+++ build/builder.go
@@ -136,8 +136,7 @@ type Builder struct {
 	// for subtree: dir -> *subtree
 	trees sync.Map
 
-	scanDepsSema *semaphore.Semaphore
-	scanDeps     *scandeps.ScanDeps
+	scanDeps *scandeps.ScanDeps
 
 	localSema *semaphore.Semaphore
 	poolSemas map[string]*semaphore.Semaphore
@@ -256,7 +255,6 @@ func New(ctx context.Context, graph Graph, opts Options) (*Builder, error) {
 		start:             start,
 		graph:             graph,
 		stepSema:          semaphore.New("step", opts.Limits.Step),
-		scanDepsSema:      semaphore.New("scandeps", opts.Limits.ScanDeps),
 		scanDeps:          scandeps.New(opts.HashFS, graph.InputDeps(ctx)),
 		localSema:         semaphore.New("localexec", opts.Limits.Local),
 		localExec:         le,
