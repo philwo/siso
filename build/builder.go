d510e34d04bc93e98f8df7113e696e1a8c4d9815
Philipp Wollermann <philwo@google.com>
2025-03-29 06:29:49 +0900

    Remove preprocSema

--- build/builder.go
+++ build/builder.go
@@ -133,8 +133,6 @@ type Builder struct {
 
 	stepSema *semaphore.Semaphore
 
-	preprocSema *semaphore.Semaphore
-
 	// for subtree: dir -> *subtree
 	trees sync.Map
 
@@ -258,7 +256,6 @@ func New(ctx context.Context, graph Graph, opts Options) (*Builder, error) {
 		start:             start,
 		graph:             graph,
 		stepSema:          semaphore.New("step", opts.Limits.Step),
-		preprocSema:       semaphore.New("preproc", opts.Limits.Preproc),
 		scanDepsSema:      semaphore.New("scandeps", opts.Limits.ScanDeps),
 		scanDeps:          scandeps.New(opts.HashFS, graph.InputDeps(ctx)),
 		localSema:         semaphore.New("localexec", opts.Limits.Local),
