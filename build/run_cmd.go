00a4b289e30e30e6ba9611875a3e5edb21988a0a
Philipp Wollermann <philwo@google.com>
2025-03-29 13:13:49 +0900

    Merge run_remote.go into run_cmd.go

--- build/run_cmd.go
+++ build/run_cmd.go
@@ -6,6 +6,13 @@ package build
 
 import (
 	"context"
+	"errors"
+	"fmt"
+
+	"github.com/charmbracelet/log"
+	"go.chromium.org/infra/build/siso/reapi"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (b *Builder) runStrategy(step *Step) func(context.Context, *Step) error {
@@ -30,4 +37,48 @@ func (b *Builder) runLocal(ctx context.Context, step *Step) error {
 	// preproc for local run.
 	dedupInputs(step.cmd)
 	return b.execLocal(ctx, step)
+}
+
+// runRemote runs step with using remote apis.
+func (b *Builder) runRemote(ctx context.Context, step *Step) error {
+	cacheCheck := b.cache != nil && b.reCacheEnableRead
+	step.setPhase(stepPreproc)
+	err := depsCmd(ctx, b, step)
+	if err != nil {
+		// disable remote execution. b/289143861
+		step.cmd.Platform = nil
+		return fmt.Errorf("disable remote: failed to get %s deps: %w", step.cmd.Deps, err)
+	}
+	dedupInputs(step.cmd)
+	err = b.runRemoteStep(ctx, step, cacheCheck)
+	if err != nil {
+		if errors.Is(err, context.Canceled) {
+			return err
+		}
+		if errors.Is(err, reapi.ErrBadPlatformContainerImage) {
+			return err
+		}
+		if status.Code(err) == codes.PermissionDenied {
+			return err
+		}
+		if errors.Is(err, errNotRelocatable) {
+			log.Errorf("not relocatable: %v", err)
+			return err
+		}
+		return fmt.Errorf("remote-exec %s failed: %w", step.cmd.ActionDigest(), err)
+	}
+	return err
+}
+
+func (b *Builder) runRemoteStep(ctx context.Context, step *Step, cacheCheck bool) error {
+	if len(step.cmd.Platform) == 0 || step.cmd.Platform["container-image"] == "" {
+		return fmt.Errorf("no remote available (missing container-image property)")
+	}
+	if cacheCheck {
+		err := b.execRemoteCache(ctx, step)
+		if err == nil {
+			return nil
+		}
+	}
+	return b.execRemote(ctx, step)
 }
