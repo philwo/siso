92702e886eb0bbbc241b0a345a6c1a874e691514
Philipp Wollermann <philwo@google.com>
2025-03-29 22:24:21 +0900

    Merge doc.go into the main source files where appropriate.

--- toolsupport/ninjautil/manifest_parser.go
+++ toolsupport/ninjautil/manifest_parser.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Package ninjautil provides utilitites for ninja.
 package ninjautil
 
 import (
