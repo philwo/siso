9ea6e071dc73a855afff0f28376b22101d7a64b6
Philipp Wollermann <philwo@google.com>
2025-03-29 12:29:18 +0900

    Remove "prepare" flag

--- build/plan.go
+++ build/plan.go
@@ -160,7 +160,6 @@ type schedulerOption struct {
 	NumTargets int
 	Path       *Path
 	HashFS     *hashfs.HashFS
-	Prepare    bool
 }
 
 // scheduler creates a plan.
@@ -175,14 +174,6 @@ type scheduler struct {
 
 	lastProgress time.Time
 	visited      int
-
-	// prepare runs steps to generate inputs for the requested targets,
-	// but not run steps to generate requested targets.
-	prepare bool
-
-	// prepareHeaderOnly runs steps to generate headers needed for
-	// the requested targets.
-	prepareHeaderOnly bool
 }
 
 func targetPath(ctx context.Context, g Graph, t Target) string {
@@ -209,26 +200,25 @@ func schedule(ctx context.Context, sched *scheduler, graph Graph, args ...string
 			continue
 		}
 
-		err := scheduleTarget(ctx, sched, graph, t, nil, sched.prepare)
+		err := scheduleTarget(ctx, sched, graph, t, nil)
 		if err != nil {
 			return fmt.Errorf("failed in schedule %s: %w", targetPath(ctx, graph, t), err)
 		}
 	}
-	if !sched.prepare {
-		for _, t := range graph.Validations() {
-			switch sched.plan.targets[t].scan {
-			case scanStateNotVisited:
-			case scanStateVisiting:
-				return fmt.Errorf("scan state %q: visiting", targetPath(ctx, graph, t))
-			case scanStateDone, scanStateIgnored:
-				continue
-			}
-			err := scheduleTarget(ctx, sched, graph, t, nil, false)
-			if err != nil {
-				return fmt.Errorf("failed in schedule %s: %w", targetPath(ctx, graph, t), err)
-			}
+	for _, t := range graph.Validations() {
+		switch sched.plan.targets[t].scan {
+		case scanStateNotVisited:
+		case scanStateVisiting:
+			return fmt.Errorf("scan state %q: visiting", targetPath(ctx, graph, t))
+		case scanStateDone, scanStateIgnored:
+			continue
+		}
+		err := scheduleTarget(ctx, sched, graph, t, nil)
+		if err != nil {
+			return fmt.Errorf("failed in schedule %s: %w", targetPath(ctx, graph, t), err)
 		}
 	}
+
 	sched.finish(time.Since(started))
 	return nil
 }
@@ -243,17 +233,13 @@ func (d DependencyCycleError) Error() string {
 }
 
 // scheduleTarget schedules a build plan for target, which is required to next StepDef, from graph into sched.
-func scheduleTarget(ctx context.Context, sched *scheduler, graph Graph, target Target, next StepDef, ignore bool) error {
+func scheduleTarget(ctx context.Context, sched *scheduler, graph Graph, target Target, next StepDef) error {
 	targets := sched.plan.targets
 	scanState := targets[target].scan
 	switch scanState {
 	case scanStateNotVisited:
 		targets[target].scan = scanStateVisiting
 		defer func() {
-			if ignore {
-				targets[target].scan = scanStateIgnored
-				return
-			}
 			targets[target].scan = scanStateDone
 		}()
 	case scanStateVisiting:
@@ -261,9 +247,6 @@ func scheduleTarget(ctx context.Context, sched *scheduler, graph Graph, target T
 			Targets: []string{targetPath(ctx, graph, target)},
 		}
 	case scanStateIgnored:
-		if ignore {
-			return nil
-		}
 		targets[target].scan = scanStateVisiting
 		defer func() {
 			targets[target].scan = scanStateDone
@@ -297,9 +280,6 @@ func scheduleTarget(ctx context.Context, sched *scheduler, graph Graph, target T
 	// mark all other outputs are done, or ignored.
 	defer func() {
 		nextState := scanStateDone
-		if ignore {
-			nextState = scanStateIgnored
-		}
 		for _, out := range outputs {
 			switch targets[out].scan {
 			case scanStateNotVisited:
@@ -315,25 +295,6 @@ func scheduleTarget(ctx context.Context, sched *scheduler, graph Graph, target T
 	case <-ctx.Done():
 		return fmt.Errorf("interrupted in schedule: %w", context.Cause(ctx))
 	default:
-	}
-
-	if ignore && sched.prepareHeaderOnly {
-		// If this step generates header (even if build dependency
-		// doesn't explicitly depend on the header), don't ignore this.
-		// b/358693473
-	outCheck:
-		for _, out := range outputs {
-			fname, err := graph.TargetPath(ctx, out)
-			if err != nil {
-				return fmt.Errorf("schedule bad target %s: %w", targetPath(ctx, graph, out), err)
-			}
-			switch filepath.Ext(fname) {
-			case ".h", ".hxx", ".hpp", ".inc":
-				ignore = false
-				break outCheck
-			}
-
-		}
 	}
 
 	// we might not need to use depfile's dependencies to construct
@@ -356,22 +317,7 @@ func scheduleTarget(ctx context.Context, sched *scheduler, graph Graph, target T
 	orderOnlyIndex := len(inputs)
 	for i, in := range append(inputs, orderOnly...) {
 		if targets[in].scan != scanStateDone {
-			// if this target is ignored, but "in" is header,
-			// then it will not ignore steps to generate "in"
-			// and "in"'s inputs recursively.
-			var inIgnore bool
-			if ignore && sched.prepareHeaderOnly {
-				fname, err := graph.TargetPath(ctx, in)
-				if err != nil {
-					return fmt.Errorf("schedule bad target %s: %w", targetPath(ctx, graph, in), err)
-				}
-				switch filepath.Ext(fname) {
-				case ".h", ".hxx", ".hpp", ".inc":
-				default:
-					inIgnore = true
-				}
-			}
-			err := scheduleTarget(ctx, sched, graph, in, next, inIgnore)
+			err := scheduleTarget(ctx, sched, graph, in, next)
 			if err != nil {
 				var cycleErr DependencyCycleError
 				if errors.As(err, &cycleErr) {
@@ -384,7 +330,7 @@ func scheduleTarget(ctx context.Context, sched *scheduler, graph Graph, target T
 				return fmt.Errorf("schedule %s: %w", targetPath(ctx, graph, in), err)
 			}
 		}
-		if !targets[in].source && !ignore {
+		if !targets[in].source {
 			// If in is not marked (i.e. source), some step
 			// will generate it, so need to wait for it
 			// before running this step.
@@ -406,9 +352,6 @@ func scheduleTarget(ctx context.Context, sched *scheduler, graph Graph, target T
 			return fmt.Errorf("schedule: non phony_output %q depends on phony_output %q", targetPath(ctx, graph, target), targetPath(ctx, graph, in))
 		}
 	}
-	if ignore {
-		return nil
-	}
 	step.outputs = outputs
 	sched.add(step)
 	return nil
@@ -416,7 +359,6 @@ func scheduleTarget(ctx context.Context, sched *scheduler, graph Graph, target T
 
 // newScheduler creates new scheduler.
 func newScheduler(opt schedulerOption) *scheduler {
-	var prepareHeaderOnly bool
 	return &scheduler{
 		path:   opt.Path,
 		hashFS: opt.HashFS,
@@ -425,8 +367,6 @@ func newScheduler(opt schedulerOption) *scheduler {
 			q:       make(chan *Step, 10000),
 			targets: make([]targetInfo, opt.NumTargets),
 		},
-		prepare:           opt.Prepare,
-		prepareHeaderOnly: prepareHeaderOnly,
 	}
 }
 
