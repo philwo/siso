964a8516c718a80e6a356785fcb2c7c495b5fc1f
Philipp Wollermann <philwo@google.com>
2025-03-29 15:42:05 +0900

    Remove REProxyConfig

--- build/ninjabuild/step_def.go
+++ build/ninjabuild/step_def.go
@@ -1057,8 +1057,3 @@ func uniqueFiles(files []string) []string {
 	}
 	return ret
 }
-
-// REProxyConfig returns configuration options for using reproxy.
-func (s *StepDef) REProxyConfig() *execute.REProxyConfig {
-	return s.rule.REProxyConfig
-}
