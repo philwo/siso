00d53de1bf8dd59b5b29ae5f688af64dc4cad6b5
Philipp Wollermann <philwo@google.com>
2025-05-06 07:03:05 +0900

    Remove execute/proto (rusage)

--- execute/proto/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2023 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Package proto provides protocol buffer message for execute.
-package proto
-
-//go:generate ../../scripts/install-protoc-gen-go protoc -I. --go_out=. --go_opt=paths=source_relative rusage.proto
