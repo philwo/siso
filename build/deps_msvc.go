1026a6fe9081ad54781de815ddf97b8871c2b6d7
Takuto Ikuta <tikuta@chromium.org>
2025-05-27 01:09:15 -0700

    siso: Derive inputs from ninja build files when necessary
    This CL takes remote inputs from ninja build files if the scapndeps
    doesn't find main source files from given command.
    
    By using specified inputs in ninja build files, we can run clang modules
    action remotely.
    
    Bug: 402356473
    Change-Id: Icccc4bc12b630e733468b18f413cd746256e9a51
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/6345875
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#73843}
    GitOrigin-RevId: e2aacbc0a67a604aa54a5d993d05d08847f6aca2

--- build/deps_msvc.go
+++ build/deps_msvc.go
@@ -55,6 +55,11 @@ func (msvc depsMSVC) fixCmdInputs(ctx context.Context, b *Builder, cmd *execute.
 		params.Sysroots[i] = b.path.MaybeFromWD(ctx, params.Sysroots[i])
 	}
 	var inputs []string
+	if len(params.Sources) == 0 {
+		// If ExtractScanDepsParams doesn't return Sources, such action uses inputs from ninja build file directly, as the action doesn't need include scanning.
+		// e.g. clang modules, rust and etc.
+		inputs = slices.Clone(cmd.Inputs)
+	}
 	// Include files detected by command line. i.e. sanitizer ignore lists.
 	// These would not be in depsfile, different from Sources.
 	inputs = append(inputs, params.Files...)
@@ -203,6 +208,12 @@ func (depsMSVC) scandeps(ctx context.Context, b *Builder, step *Step) ([]string,
 			step.metrics.ActionStartTime = IntervalMetric(time.Since(b.start))
 		}
 		params := msvcutil.ExtractScanDepsParams(ctx, step.cmd.Args, step.cmd.Env)
+		if len(params.Sources) == 0 {
+			// If ExtractScanDepsParams doesn't return Sources, such action uses inputs from ninja build file directly, as the action doesn't need include scanning.
+			// e.g. clang modules, rust and etc.
+			return nil
+		}
+
 		for i := range params.Sources {
 			params.Sources[i] = b.path.MaybeFromWD(ctx, params.Sources[i])
 		}
