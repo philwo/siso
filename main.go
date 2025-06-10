1f36c885cebaacbb95cbdc9fad581a4b16745a47
Fumitoshi Ukai <ukai@google.com>
2025-06-09 18:17:27 -0700

    siso: update version string to v1.2.9
    v1.2.8 has been rolled out as pinned version at
    git_revision:d704490133011610c402696584afedea80829dbd
    https://chromium-review.googlesource.com/c/chromium/src/+/6626722
    
    Bug: b/421771552
    Change-Id: If55d0448baa58d6d18ace8de5d7f58c9e248e03d
    Reviewed-on: https://chromium-review.googlesource.com/c/infra/infra/+/6629893
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Cr-Commit-Position: refs/heads/main@{#74172}
    GitOrigin-RevId: 3c379ee3663bf92c24d566dfe9cb01822917f670

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ var (
 	traceFile     string
 )
 
-const versionID = "v1.2.8"
+const versionID = "v1.2.9"
 const versionStr = "siso " + versionID
 
 func getApplication(authOpts cred.Options) *cli.Application {
